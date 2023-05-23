0eadfd3fe3e2e3cbde20eeaeafe567c6c70ef8e0
Simon Birrer <sibirrer@users.noreply.github.com>
2023-05-23 17:00:35 -0400

    no reliance on a branch on skypy

--- sim_pipeline/Lenses/velocity_dispersion.py
+++ sim_pipeline/Lenses/velocity_dispersion.py
@@ -1,7 +1,10 @@
 import numpy as np
 import scipy
 from skypy.galaxies.redshift import redshifts_from_comoving_density
-from skypy.galaxies.velocity_dispersion import schechter_vdf
+from skypy.utils.random import schechter
+
+#  TODO: some functionality may directly be imported from skypy, once a version is released
+#  from skypy.galaxies.velocity_dispersion import schechter_vdf
 
 
 def schechter_vel_disp(redshift, phi_star, alpha, beta, vd_star, vd_min, vd_max, sky_area, cosmology, noise=True):
@@ -152,3 +155,61 @@ def schechter_vel_disp_redshift(redshift, phi_star, alpha, beta, vd_star, vd_min
     # sample redshifts from the comoving density
     return redshifts_from_comoving_density(redshift=redshift, density=density,
                                            sky_area=sky_area, cosmology=cosmology, noise=noise)
+
+
+def schechter_vdf(alpha, beta, vd_star, vd_min, vd_max, size=None, resolution=1000):
+    r"""Sample velocity dispersion of elliptical galaxies in the local universe
+    following a Schecter function.
+
+    Parameters
+    ----------
+    alpha: int
+        The alpha parameter in the modified Schechter equation.
+    beta: int
+        The beta parameter in the modified Schechter equation.
+    vd_star: int
+        The characteristic velocity dispersion.
+    vd_min, vd_max: int
+        Lower and upper bounds of random variable x. Samples are drawn uniformly from bounds.
+    resolution: int
+        Resolution of the inverse transform sampling spline. Default is 100.
+    size: int
+        Number of samples returned. Default is 1.
+
+    Returns
+    -------
+    velocity_dispersion: array_like
+        Velocity dispersion drawn from Schechter function.
+
+    Notes
+    -----
+    The probability distribution function :math:`p(\sigma)` for velocity dispersion :math:`\sigma`
+    can be described by a Schechter function (see eq. (4) in [1]_)
+
+    .. math::
+
+        \phi = \phi_* \left(\frac{\sigma}{\sigma_*}\right)^\alpha
+            \exp\left[-\left( \frac{\sigma}{\sigma_*} \right)^\beta\right]
+            \frac{\beta}{\Gamma(\alpha/\beta)} \frac{1}{\sigma} \mathrm{d}\sigma \;.
+
+    where :math:`\Gamma` is the gamma function, :math:`\sigma_*` is the
+    characteristic velocity dispersion, :math:`\phi_*` is
+    number density of all spiral galaxies and
+    :math:`\alpha` and :math:`\beta` are free parameters.
+
+    References
+    ----------
+    .. [1] Choi, Park and Vogeley, (2007), astro-ph/0611607, doi:10.1086/511060
+
+    """
+
+    if np.ndim(alpha) > 0:
+        raise NotImplementedError('only scalar alpha is supported')
+
+    alpha_prime = alpha/beta - 1
+    x_min, x_max = (vd_min/vd_star)**beta, (vd_max/vd_star)**beta
+
+    samples = schechter(alpha_prime, x_min, x_max, resolution=resolution, size=size)
+    samples = samples**(1/beta) * vd_star
+
+    return samples
