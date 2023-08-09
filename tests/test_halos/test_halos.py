b78f6386cb4439224e7706ad42bb086677b0ff5e
trivialTZ <122687108+trivialTZ@users.noreply.github.com>
2023-08-09 10:08:39 -0400

    Make it past test

--- tests/test_halos/test_halos.py
+++ tests/test_halos/test_halos.py
@@ -2,15 +2,18 @@ from astropy.cosmology import FlatLambdaCDM
 import numpy as np
 from sim_pipeline.halos.halos import number_density_at_redshift, growth_factor_at_redshift, halo_mass_at_z
 
+
 def test_halo_mass_at_z():
-    mass = halo_mass_at_z(z=0.5,resolution=100)
-    assert mass[0] > 10**10
-    assert mass[0] < 10**14
+    mass = halo_mass_at_z(z=0.5, resolution=100)
+    assert mass[0] > 10 ** 10
+    assert mass[0] < 10 ** 16
+
 
 def test_number_density_at_redshift():
     z = 0.5
-    CDF = number_density_at_redshift(z= z)
+    CDF = number_density_at_redshift(z=z)
     assert CDF is not None
+
 
 def test_growth_factor_at_redshift():
     growth_factor = growth_factor_at_redshift(z=0.5)
