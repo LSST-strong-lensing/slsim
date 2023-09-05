6d0e9dbb5dd4b0defdd7b02e7ab22c5e23528dd7
narayan <nkhadka@ksu.edu>
2023-09-05 17:15:50 -0400

    minor changes

--- tests/test_Source/test_galaxies.py
+++ tests/test_Source/test_galaxies.py
@@ -11,7 +11,8 @@ class test_galaxies(object):
             pipeline = SkyPyPipeline(skypy_config=None, sky_area=sky_area, filters=None)
             self.galaxy_list = pipeline.red_galaxies
             self.cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
-            self.galaxies = Galaxies(galaxy_list = self.galaxy_list, kwargs_cut = None, cosmo = self.cosmo, sky_area = sky_area)
+            self.galaxies = Galaxies(galaxy_list = self.galaxy_list, kwargs_cut = None, 
+                                     cosmo = self.cosmo, sky_area = sky_area)
 
             
         def test_source_number(self):
