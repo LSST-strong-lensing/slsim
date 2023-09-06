f64cc8b442f22b296db211e2c83c6108a55e9b86
narayan <nkhadka@ksu.edu>
2023-09-06 14:42:08 -0400

    removed galsimobj test from test_image_simulation.py

--- tests/test_image_simulation.py
+++ tests/test_image_simulation.py
@@ -2,8 +2,8 @@ import os
 from astropy.table import Table
 from astropy.cosmology import FlatLambdaCDM
 from sim_pipeline.galaxy_galaxy_lens import GalaxyGalaxyLens
-from sim_pipeline.image_simulation import (simulate_image, sharp_image, galsimobj, 
-                                        galsimobj_true_flux, sharp_rgb_image, rgb_image_from_image_list)
+from sim_pipeline.image_simulation import (simulate_image, sharp_image, sharp_rgb_image, 
+                                           rgb_image_from_image_list)
 
 
 class TestImageSimulation(object):
@@ -33,18 +33,6 @@ class TestImageSimulation(object):
         image = sharp_image(lens_class=self.gg_lens, band='g', mag_zero_point=30, delta_pix=0.05, num_pix=100,
                             with_deflector=True)
         assert len(image) == 100
-
-    def test_galsimobj(self):
-        image = sharp_image(lens_class=self.gg_lens, band='g', mag_zero_point=30, delta_pix=0.05, num_pix=100,
-                            with_deflector=True)
-        gsobj = galsimobj(image, 0.2, 10000)
-        assert len(gsobj.image.array) == 100
-
-    def test_galsimobj_true_flux(self):
-        image = sharp_image(lens_class=self.gg_lens, band='g', mag_zero_point=30, delta_pix=0.05, num_pix=100,
-                            with_deflector=True)
-        gsobj = galsimobj_true_flux(image, 0.2)
-        assert len(gsobj.image.array) == 100
 
     def test_sharp_rgb_image(self):
         image = sharp_rgb_image(lens_class = self.gg_lens, rgb_band_list = ['r', 'g', 'i'], mag_zero_point = 30,
