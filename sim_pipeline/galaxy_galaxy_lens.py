73282134154f14d00e7f6013f4f827686fd58cb3
Narayan Khadka <knarayan47.14@gmail.com>
2023-09-12 12:36:11 -0400

    Update galaxy_galaxy_lens.py

--- sim_pipeline/galaxy_galaxy_lens.py
+++ sim_pipeline/galaxy_galaxy_lens.py
@@ -326,7 +326,8 @@ class GalaxyGalaxyLens(object):
         band_string = str("mag_" + band)
         # TODO: might have to change conventions between extended and point source
         source_mag = self._source_dict[band_string]
-        # TODO: requires time information and needs to be shifted for different arriving images
+        # TODO: requires time information and needs to be shifted for 
+        # different arriving images
         if lensed:
             mag = self.point_source_magnification()
             return source_mag - 2.5 * np.log10(np.abs(mag))
