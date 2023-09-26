ebbb04cc697ab714dc1c04171ae690951d273f5c
narayan <nkhadka@ksu.edu>
2023-09-26 19:25:44 -0400

    minor change

--- tests/test_Source/test_quasar.py
+++ tests/test_Source/test_quasar.py
@@ -5,26 +5,24 @@ from sim_pipeline.Sources.quasar_catalog.simple_quasar import quasar_catalog
 import pytest
 
 
-class test_quasar(object):
-    def setup_method(self):
-        sky_area = Quantity(value=0.1, unit="deg2")
-        self.quasar_list = quasar_catalog(50000, 0.1, 5, 17, 23)
-        self.cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
-        self.quasar = Quasars(
-            quasar_list=self.quasar_list,
-            kwargs_cut=None,
-            cosmo=self.cosmo,
-            sky_area=sky_area,
-        )
+@pytest.fixture
+def Quasar_class():
+    sky_area = Quantity(value=0.1, unit="deg2")
+    quasar_list = quasar_catalog(50000, 0.1, 5, 17, 23)
+    cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+    return Quasars(
+        quasar_list=quasar_list,
+        kwargs_cut=None,
+        cosmo=cosmo,
+        sky_area=sky_area)
 
-    def test_source_number(self):
-        number = self.quasar.source_number()
-        assert number > 0
+def test_source_number():
+    number = Quasar_class.source_number()
+    assert number > 0
 
-    def test_draw_source(self):
-        quasar = self.quasar.draw_source()
-        assert len(quasar) > 0
-        assert len(self.quasar_list) == 50000
+def test_draw_source():
+    quasar = Quasar_class.draw_source()
+    assert len(quasar) > 0
 
 if __name__ == "__main__":
     pytest.main()
