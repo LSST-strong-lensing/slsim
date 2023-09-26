bcd35f453aaea0634e683625ecb07dc31e59f887
narayan <nkhadka@ksu.edu>
2023-09-26 18:52:50 -0400

    minor change

--- tests/test_Source/test_quasar.py
+++ tests/test_Source/test_quasar.py
@@ -1,6 +1,6 @@
 from astropy.cosmology import FlatLambdaCDM
 from astropy.units import Quantity
-from sim_pipeline.Sources.quasars import Quasar
+from sim_pipeline.Sources.quasars import Quasars
 from sim_pipeline.Sources.quasar_catalog.simple_quasar import quasar_catalog
 import pytest
 
@@ -10,7 +10,7 @@ class test_quasar(object):
         sky_area = Quantity(value=0.1, unit="deg2")
         self.quasar_list = quasar_catalog(50000, 0.1, 5, 17, 23)
         self.cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
-        self.quasar = Quasar(
+        self.quasar = Quasars(
             galaxy_list=self.quasar_list,
             kwargs_cut=None,
             cosmo=self.cosmo,
