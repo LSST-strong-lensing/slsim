2843970590d7c29634cb4f7b04f551a1b1537b6f
narayan <nkhadka@ksu.edu>
2023-09-26 18:48:41 -0400

    added a test function for variability

---
+++ tests/test_Source/test_source_variability.py
@@ -0,0 +1,5 @@
+from sim_pipeline.Sources.source_variability.variability import sinusoidal_variability
+
+def test_sinusoidal_variability():
+    variability = sinusoidal_variability(0)
+    assert variability == 0
