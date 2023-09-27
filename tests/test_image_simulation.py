aa4a3ba0aef8ce725bd0c2e272a3564e07007a96
narayan <nkhadka@ksu.edu>
2023-09-27 11:43:56 -0400

    minor change

--- tests/test_image_simulation.py
+++ tests/test_image_simulation.py
@@ -2,14 +2,17 @@ import os
 from astropy.table import Table
 from astropy.cosmology import FlatLambdaCDM
 from sim_pipeline.galaxy_galaxy_lens import GalaxyGalaxyLens
+from sim_pipeline.galaxy_galaxy_lens_pop import GalaxyGalaxyLensPop
+from astropy.units import Quantity
 from sim_pipeline.image_simulation import (
     simulate_image,
     sharp_image,
     sharp_rgb_image,
-    rgb_image_from_image_list,
+    rgb_image_from_image_list, point_source_image_properties
 )
+import pytest
 
-
+@pytest.fixture
 class TestImageSimulation(object):
     def setup_method(self):
         # path = os.path.dirname(sim_pipeline.__file__)
@@ -95,3 +98,34 @@ class TestImageSimulation(object):
         image_list = [image_r, image_g, image_b]
         image = rgb_image_from_image_list(image_list, 0.5)
         assert len(image) == 100
+
+def quasar_lens_pop_instance():
+    cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+    sky_area = Quantity(value=0.1, unit="deg2")
+    return GalaxyGalaxyLensPop(deflector_type="all-galaxies",
+    source_type="quasars",
+    kwargs_deflector_cut=None,
+    kwargs_source_cut=None,
+    kwargs_mass2light=None,
+    skypy_config=None,
+    sky_area=sky_area,
+    cosmo=cosmo)
+
+def test_point_source_image_properties(quasar_lens_pop_instance):
+    kwargs_lens_cut={'min_image_separation': 0.8, 'max_image_separation': 10}
+    lens_class = quasar_lens_pop_instance.select_lens_at_random(**kwargs_lens_cut)
+    result = point_source_image_properties(lens_class = lens_class, band = 'i', 
+                        mag_zero_point = 27, delta_pix = 0.2, num_pix = 101)
+
+    expected_columns = ["deflector_pix", "image_pix", "ra_image", "dec_image",
+                            "image_amplitude", "image_magnitude", "radec_at_xy_0"]
+    assert result.colnames[0] == expected_columns[0]
+    assert result.colnames[1] == expected_columns[1]
+    assert result.colnames[2] == expected_columns[2]
+    assert result.colnames[3] == expected_columns[3]
+    assert result.colnames[4] == expected_columns[4]
+    assert result.colnames[5] == expected_columns[5]
+    assert result.colnames[6] == expected_columns[6]
+
+if __name__ == "__main__":
+    pytest.main()
