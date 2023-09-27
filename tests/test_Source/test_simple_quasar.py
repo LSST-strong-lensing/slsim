4e5d30a5d221a01be0d1351e014b3f00ab30be3a
narayan <nkhadka@ksu.edu>
2023-09-27 18:36:28 -0400

    minor change

--- tests/test_Source/test_simple_quasar.py
+++ tests/test_Source/test_simple_quasar.py
@@ -3,7 +3,8 @@ import pytest
 
 
 def test_simple_quasar():
-    catalog = simple_quasar.quasar_catalog(n = 1000, z_min = 0.1, z_max = 5, m_min = 17, m_max = 23)
+    catalog = simple_quasar.quasar_catalog(n = 1000, z_min = 0.1, z_max = 5, 
+                                           m_min = 17, m_max = 23)
     column_names = ["z", "mag_r", "mag_g", "mag_i"]
     assert catalog.colnames[0] == column_names[0]
     assert catalog.colnames[1] == column_names[1]
