f6e44f0b7436ea2f443056669e06c7768df9a4c6
narayan <nkhadka@ksu.edu>
2023-09-27 18:05:17 -0400

    minor change

--- tests/test_Plots/test_plot_functions.py
+++ tests/test_Plots/test_plot_functions.py
@@ -5,8 +5,7 @@ from sim_pipeline.galaxy_galaxy_lens_pop import GalaxyGalaxyLensPop
 from sim_pipeline.Sources.source_variability.variability import sinusoidal_variability
 from astropy.units import Quantity
 from sim_pipeline.Plots.plot_functions import plot_montage_of_random_injected_lens
-from sim_pipeline.image_simulation import (point_source_image_properties, 
-                                           point_source_image)
+from sim_pipeline.image_simulation import sharp_image
 import pytest
 
 @pytest.fixture
@@ -26,26 +25,14 @@ def quasar_lens_pop_instance():
 
 def test_plot_montage_of_random_injected_lens(quasar_lens_pop_instance):
     kwargs_lens_cut={'min_image_separation': 0.8, 'max_image_separation': 10}
-    lens_class = quasar_lens_pop_instance.select_lens_at_random(**kwargs_lens_cut)
-    image_data = point_source_image_properties(lens_class = lens_class, band = 'i', 
-                        mag_zero_point = 27, delta_pix = 0.2, num_pix = 101)
-    number = len(image_data['image_pix'])
-    path = os.path.dirname(__file__)
-    psf=np.loadtxt(os.path.join(path, "TestData/psf_kernels_for_deflector.txt"), unpack = True)
-    psf_image_1 = [psf]
-    psf_kernels = psf_image_1[:-1]
-    psf_kernels.extend([psf_image_1[-1]] * number)
+    image_list = []
+    for i in range(4):
+        lens_class = quasar_lens_pop_instance.select_lens_at_random(**kwargs_lens_cut)
+        image_list.append(sharp_image(lens_class=lens_class, band='i', 
+                                mag_zero_point=27, delta_pix=0.2, num_pix=101))
 
-    time = np.linspace(0, 10, 4)
-    variability = {'time': time, 
-        'function': sinusoidal_variability}
-    # Call the function to get the result
-    result1 = point_source_image(lens_class = lens_class, band = 'i', 
-            mag_zero_point = 27, delta_pix = 0.2, num_pix = 101, 
-            psf_kernels = psf_kernels, variability = variability,
-                lensed=True)
     plots = plot_montage_of_random_injected_lens(
-        image_list = result1[0], num = len(result1[0]),
+        image_list = image_list, num = 4,
         n_horizont=2,
         n_vertical=2)
     assert plots is None
