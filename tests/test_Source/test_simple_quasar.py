448b474edc40b5abbfe63fc7d1fef61d6e74945c
narayan <nkhadka@ksu.edu>
2023-09-27 18:28:23 -0400

    added a test function for simple_quasar.py

---
+++ tests/test_Source/test_simple_quasar.py
@@ -0,0 +1,14 @@
+from sim_pipeline.Sources.quasar_catalog import simple_quasar
+import pytest
+
+
+def test_simple_quasar():
+    catalog = simple_quasar.quasar_catalog(n = 1000, z_min = 0.1, z_max = 5, m_min = 17, m_max = 23)
+    column_names = ["z", "mag_r", "mag_g", "mag_i"]
+    assert catalog.colnames[0] == column_names[0]
+    assert catalog.colnames[1] == column_names[1]
+    assert catalog.colnames[2] == column_names[2]
+    assert catalog.colnames[3] == column_names[3]
+
+if __name__ == "__main__":
+    pytest.main()
