734a9c323dd2d7d97a3876cef39ed2d065a0d694
narayan <nkhadka@ksu.edu>
2023-09-27 12:39:26 -0400

    added a test function for point source image.

--- tests/test_image_simulation.py
+++ tests/test_image_simulation.py
@@ -1,14 +1,16 @@
 import os
+import numpy as np
 from astropy.table import Table
 from astropy.cosmology import FlatLambdaCDM
 from sim_pipeline.galaxy_galaxy_lens import GalaxyGalaxyLens
 from sim_pipeline.galaxy_galaxy_lens_pop import GalaxyGalaxyLensPop
+from sim_pipeline.Sources.source_variability.variability import sinusoidal_variability
 from astropy.units import Quantity
 from sim_pipeline.image_simulation import (
     simulate_image,
     sharp_image,
     sharp_rgb_image,
-    rgb_image_from_image_list, point_source_image_properties
+    rgb_image_from_image_list, point_source_image_properties, point_source_image
 )
 import pytest
 
@@ -126,6 +128,36 @@ def test_point_source_image_properties(quasar_lens_pop_instance):
     assert result.colnames[4] == expected_columns[4]
     assert result.colnames[5] == expected_columns[5]
     assert result.colnames[6] == expected_columns[6]
+
+def test_point_source_image(quasar_lens_pop_instance):
+    kwargs_lens_cut={'min_image_separation': 0.8, 'max_image_separation': 10}
+    lens_class = quasar_lens_pop_instance.select_lens_at_random(**kwargs_lens_cut)
+    image_data = point_source_image_properties(lens_class = lens_class, band = 'i', 
+                        mag_zero_point = 27, delta_pix = 0.2, num_pix = 101)
+    number = len(image_data['image_pix'])
+    psf_image_1 = [np.loadtxt('/data/dp0/psf_kernels_for_image_1.txt', unpack = True)]
+    psf_kernels = psf_image_1[:-1]
+    psf_kernels.extend([psf_image_1[-1]] * number)
+    
+    #psf_image_2 = np.loadtxt('/data/dp0/psf_kernels_for_image_2.txt', unpack = True)
+    #psf_deflector = np.loadtxt('/data/dp0/psf_kernels_for_deflector.txt',
+                                #unpack = True)
+    #psf_kernels = [psf_image_1, psf_image_2, psf_deflector]
+
+    time = np.linspace(0, 10, 3)
+    variability = {'time': time, 
+        'function': sinusoidal_variability}
+    # Call the function to get the result
+    result1 = point_source_image(lens_class = lens_class, band = 'i', 
+            mag_zero_point = 27, delta_pix = 0.2, num_pix = 101, 
+            psf_kernels = psf_kernels, variability = variability,
+                lensed=True)
+    result2 = point_source_image(lens_class = lens_class, band = 'i', 
+            mag_zero_point = 27, delta_pix = 0.2, num_pix = 101, 
+            psf_kernels = psf_kernels, variability = None,
+                lensed=True)
+    assert len(result1[0]) == len(time)
+    assert len(result2) == number
 
 if __name__ == "__main__":
     pytest.main()
