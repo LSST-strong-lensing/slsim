843968e2f841dfe0b7867e74dc2bc7ec21cdc057
trivialTZ <122687108+trivialTZ@users.noreply.github.com>
2023-09-29 12:43:09 -0400

    Add cosmology support in the galaxies pipeline
    - Enhanced pipeline configuration to support various cosmology models.
    - Ensured compatibility with the existing pipeline structure and functionality.
    - Enhanced the `GalaxyGalaxyLensPop` class to accept dynamic cosmology inputs.
    - Fixed issues ensuring the cosmological input directly influences galaxy generation.

--- sim_pipeline/Pipelines/skypy_pipeline.py
+++ sim_pipeline/Pipelines/skypy_pipeline.py
@@ -7,23 +7,23 @@ import tempfile
 class SkyPyPipeline:
     """Class for skypy configuration."""
 
-    def __init__(self, skypy_config=None, sky_area=None, filters=None):
+    def __init__(self, skypy_config=None, sky_area=None, filters=None, cosmo=None):
         """
-        :param skypy_config: path to SkyPy configuration yaml file. If None, uses
-            'data/SkyPy/lsst-like.yml'.
+        :param skypy_config: path to SkyPy configuration yaml file. If None, uses 'data/SkyPy/lsst-like.yml'.
         :type skypy_config: string or None
         :type sky_area: `~astropy.units.Quantity`
-        :param sky_area: Sky area over which galaxies are sampled. Must be in units of
-            solid angle.
+        :param sky_area: Sky area over which galaxies are sampled. Must be in units of solid angle.
         :param filters: filters for SED integration
         :type filters: list of strings or None
+        :param cosmo: An instance of an astropy cosmology model (e.g., FlatLambdaCDM(H0=70, Om0=0.3)).
+        :type cosmo: astropy.cosmology.FLRW instance or None
         """
         path = os.path.dirname(sim_pipeline.__file__)
         module_path, _ = os.path.split(path)
         if skypy_config is None:
             skypy_config = os.path.join(module_path, "data/SkyPy/lsst-like.yml")
 
-        if sky_area is None and filters is None:
+        if sky_area is None and filters is None and cosmo is None:
             self._pipeline = Pipeline.read(skypy_config)
             self._pipeline.execute()
         else:
@@ -35,8 +35,30 @@ class SkyPyPipeline:
                 new_fsky = f"fsky: {sky_area.value} {sky_area.unit}"
                 content = content.replace(old_fsky, new_fsky)
 
+            if cosmo:
+                cosmology_model_name = cosmo.__class__.__name__
+
+                cosmology_params_list = []
+                for param in cosmo.__parameters__:
+                    value = getattr(cosmo, param)
+                    if value is None:
+                        continue
+                    if isinstance(value, (int, float)):
+                        cosmology_params_list.append(f"    {param}: {value}")
+                    elif hasattr(value, "value"):
+                        cosmology_params_list.append(f"    {param}: {value.value}")
+                    else:
+                        cosmology_params_list.append(f"    {param}: {value}")
+
+                cosmology_params_str = "\n".join(cosmology_params_list)
+
+                old_cosmo = "cosmology: !astropy.cosmology.default_cosmology.get []"
+                new_cosmo = f"cosmology: !astropy.cosmology.{cosmology_model_name}\n{cosmology_params_str}"
+                print(new_cosmo)
+                content = content.replace(old_cosmo, new_cosmo)
+
             with tempfile.NamedTemporaryFile(
-                mode="w", delete=False, suffix=".yml"
+                    mode="w", delete=False, suffix=".yml"
             ) as tmp_file:
                 tmp_file.write(content)
 
@@ -47,7 +69,8 @@ class SkyPyPipeline:
             os.remove(tmp_file.name)
         # TODO: note that the f_sky can not be set to large. Need to figure out
         #  how to do this properly
-        # for LSST simulations (10^5 deg^2)
+
+        # TODO: make filters work
 
     @property
     def blue_galaxies(self):
--- tests/test_Pipelines/test_skypy_pipeline.py
+++ tests/test_Pipelines/test_skypy_pipeline.py
@@ -1,4 +1,5 @@
 from sim_pipeline.Pipelines.skypy_pipeline import SkyPyPipeline
+from astropy.cosmology import LambdaCDM, FlatwCDM, w0waCDM
 
 
 class TestSkyPyPipeline(object):
@@ -7,6 +8,30 @@ class TestSkyPyPipeline(object):
 
         sky_area = Quantity(value=0.05, unit="deg2")
         self.pipeline = SkyPyPipeline(skypy_config=None, sky_area=sky_area)
+
+    def test_cosmology_initialization(self):
+        # LambdaCDM model
+        cosmo1 = LambdaCDM(H0=70, Om0=0.3, Ode0=0.7)
+        pipeline1 = SkyPyPipeline(cosmo=cosmo1)
+        blue_galaxies = pipeline1.blue_galaxies
+        assert blue_galaxies[0]["z"] > 0
+
+        cosmo2 = LambdaCDM(H0=70, Om0=0.3, Ode0=0.7)
+        pipeline2 = SkyPyPipeline(cosmo=cosmo2)
+        red_galaxies = pipeline2.red_galaxies
+        assert red_galaxies[0]["z"] > 0
+
+        # FlatwCDM model
+        cosmo3 = FlatwCDM(H0=70, Om0=0.3, w0=-1.0)
+        pipeline3 = SkyPyPipeline(cosmo=cosmo3)
+        red_galaxies = pipeline3.red_galaxies
+        assert red_galaxies[0]["z"] > 0
+
+        # w0waCDM model
+        cosmo4 = w0waCDM(H0=70, Om0=0.3, Ode0=0.7, w0=-1.0, wa=0.0)
+        pipeline4 = SkyPyPipeline(cosmo=cosmo4)
+        blue_galaxies = pipeline4.blue_galaxies
+        assert blue_galaxies[0]["z"] > 0
 
     def test_blue_galaxies(self):
         blue_galaxies = self.pipeline.blue_galaxies
