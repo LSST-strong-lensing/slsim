458510115200e89b2f9ed479ecfeb3b731071313
trivialTZ <122687108+trivialTZ@users.noreply.github.com>
2023-10-02 13:48:23 -0400

    Update test_skypy_pipeline.py
    add more annotation to the test function.

--- tests/test_Pipelines/test_skypy_pipeline.py
+++ tests/test_Pipelines/test_skypy_pipeline.py
@@ -22,22 +22,22 @@ class TestSkyPyPipeline(object):
         pipeline1 = SkyPyPipeline(cosmo=cosmo1)
         blue_galaxies = pipeline1.blue_galaxies
         red_galaxies = pipeline1.red_galaxies
-        assert len(blue_galaxies) * 2 < cosmo0_blue_number
-        assert len(red_galaxies) * 2 < cosmo0_red_number
+        # we expect the pipeline works:
         assert blue_galaxies[0]["z"] > 0
-
-        cosmo2 = LambdaCDM(H0=70, Om0=0.3, Ode0=0.7)
-        pipeline2 = SkyPyPipeline(cosmo=cosmo2)
-        red_galaxies = pipeline2.red_galaxies
         assert red_galaxies[0]["z"] > 0
 
-        # FlatwCDM model
+        # we expect the galaxy number from this cosmo is significantly
+        # less than the previous cosmo, indicating change of cosmology:
+        assert len(blue_galaxies) * 2 < cosmo0_blue_number
+        assert len(red_galaxies) * 2 < cosmo0_red_number
+
+        # check FlatwCDM model
         cosmo3 = FlatwCDM(H0=70, Om0=0.3, w0=-1.0)
         pipeline3 = SkyPyPipeline(cosmo=cosmo3)
         red_galaxies = pipeline3.red_galaxies
         assert red_galaxies[0]["z"] > 0
 
-        # w0waCDM model
+        # check w0waCDM model
         cosmo4 = w0waCDM(H0=70, Om0=0.3, Ode0=0.7, w0=-1.0, wa=0.0)
         pipeline4 = SkyPyPipeline(cosmo=cosmo4)
         blue_galaxies = pipeline4.blue_galaxies
