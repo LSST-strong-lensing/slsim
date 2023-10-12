d1634e6ec2950dbedc9505caed8b44b8df50aa49
narayan <nkhadka@ksu.edu>
2023-10-12 15:49:46 -0400

    minor change

--- tests/test_image_simulation.py
+++ tests/test_image_simulation.py
@@ -11,7 +11,7 @@ from sim_pipeline.image_simulation import (
     sharp_image,
     sharp_rgb_image,
     rgb_image_from_image_list,
-    point_source_image_properties,
+    point_source_coordinate_properties,
     point_source_image,
 )
 import pytest
@@ -108,9 +108,12 @@ class TestImageSimulation(object):
 def quasar_lens_pop_instance():
     cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
     sky_area = Quantity(value=0.1, unit="deg2")
+    kwargs_variability = {
+        "variability_model": "sinusoidal", "amp": 1.0, "freq": 0.5}
     return GalaxyGalaxyLensPop(
         deflector_type="all-galaxies",
         source_type="quasars",
+        kwargs_variability=kwargs_variability,
         kwargs_deflector_cut=None,
         kwargs_source_cut=None,
         kwargs_quasars={
@@ -130,7 +133,7 @@ def quasar_lens_pop_instance():
 def test_point_source_image_properties(quasar_lens_pop_instance):
     kwargs_lens_cut = {"min_image_separation": 0.8, "max_image_separation": 10}
     lens_class = quasar_lens_pop_instance.select_lens_at_random(**kwargs_lens_cut)
-    result = point_source_image_properties(
+    result = point_source_coordinate_properties(
         lens_class=lens_class, band="i", mag_zero_point=27, delta_pix=0.2, num_pix=101
     )
 
@@ -139,8 +142,6 @@ def test_point_source_image_properties(quasar_lens_pop_instance):
         "image_pix",
         "ra_image",
         "dec_image",
-        "image_amplitude",
-        "image_magnitude",
         "radec_at_xy_0",
     ]
     assert result.colnames[0] == expected_columns[0]
@@ -148,14 +149,12 @@ def test_point_source_image_properties(quasar_lens_pop_instance):
     assert result.colnames[2] == expected_columns[2]
     assert result.colnames[3] == expected_columns[3]
     assert result.colnames[4] == expected_columns[4]
-    assert result.colnames[5] == expected_columns[5]
-    assert result.colnames[6] == expected_columns[6]
 
 
 def test_point_source_image(quasar_lens_pop_instance):
     kwargs_lens_cut = {"min_image_separation": 0.8, "max_image_separation": 10}
     lens_class = quasar_lens_pop_instance.select_lens_at_random(**kwargs_lens_cut)
-    image_data = point_source_image_properties(
+    image_data = point_source_coordinate_properties(
         lens_class=lens_class, band="i", mag_zero_point=27, delta_pix=0.2, num_pix=101
     )
     number = len(image_data["image_pix"])
@@ -179,7 +178,7 @@ def test_point_source_image(quasar_lens_pop_instance):
         delta_pix=0.2,
         num_pix=101,
         psf_kernels=psf_kernels,
-        variability=variability,
+        time=time,
     )
     result2 = point_source_image(
         lens_class=lens_class,
@@ -188,7 +187,6 @@ def test_point_source_image(quasar_lens_pop_instance):
         delta_pix=0.2,
         num_pix=101,
         psf_kernels=psf_kernels,
-        variability=None,
     )
 
     assert len(result1[0]) == len(time)
