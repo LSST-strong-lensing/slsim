405e19b5a9728ca0f28afe6ac42618f961f1189d
Patrick Wells <pwells@ucdavis.edu>
2023-10-27 10:32:16 -0700

    Move to using pydantic.PositiveFloat

--- slsim/ParamDistributions/params.py
+++ slsim/ParamDistributions/params.py
@@ -1,12 +1,10 @@
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, Field, PositiveFloat, model_validator
 import numpy as np
 
 class GaussianMixtureModel(BaseModel):
     means: list[float] = [0.00330796, -0.07635054, 0.11829008]
-    stds: list[float] = Field(gt=0, 
-                              default=[np.sqrt(0.00283885), np.sqrt(0.01066668), 
-                                       np.sqrt(0.0097978)])
-    weights: list[float] = [0.62703102, 0.23732313, 0.13564585]
+    stds: list[PositiveFloat] = [np.sqrt(0.00283885), np.sqrt(0.01066668), np.sqrt(0.0097978)]
+    weights: list[PositiveFloat] = [0.62703102, 0.23732313, 0.13564585]
     
     @model_validator(mode="after")
     def check_lenghts(self):
