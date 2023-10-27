3db5362c968a69ce2dfc58514990a206f05859f5
Patrick Wells <pwells@ucdavis.edu>
2023-10-27 10:10:55 -0700

    add docstrings

--- slsim/Util/params.py
+++ slsim/Util/params.py
@@ -13,7 +13,21 @@ class SlSimParameterException(Exception):
 
 _defaults = {}
 
-def check_params(init_fn):
+def check_params(init_fn: Callable) -> Callable:
+    """
+    A decorator for enforcing checking of params in __init__ methods. This
+    decorator will automatically load the default parameters for the class
+    and check that the passed parameters are valid. It expeects a "params.py"
+    file in the same folder as the class definition. Uses pydantic models
+    to enforce types, sanity checks, and defaults.
+
+    From and end user perspective, there is no difference between this and a normal
+    __init__ fn. Developers only need to add @check_params above their __init__
+    method definition to enable this feature, then add their default parameters
+    to the "params.py" file.
+    """
+
+
     if not init_fn.__name__.startswith('__init__'):
         raise SlSimParameterException('pcheck decorator can currently only be used'\
                                     ' with__init__ methods')
@@ -39,15 +53,23 @@ def get_defaults(init_fn):
     modpath = path[start:].split('/')
     modpath = modpath[1:-1] + ["params"]
     modpath = ".".join(["slsim"] + modpath)
+    # Unfortunately, there doesn't seem to be a better way of doing this.
+
     if modpath not in _defaults:
-        _defaults[modpath] = load_defaults(modpath, obj_name)
+        #Little optimization. We cache defaults so we don't have to reload them
+        # every time we construct a new object.
+        _defaults[modpath] = load_parameters(modpath, obj_name)
     return _defaults[modpath]
 
-def load_defaults(modpath, obj_name):
+def load_parameters(modpath, obj_name):
+    """
+    Loads parameters from the "params.py" file which should be in the same folder
+    as the class definition.
+    """
     try:
         defaults = import_module(modpath)
     except ModuleNotFoundError:
-        raise SlSimParameterException('No default parameters found for module '\
+        raise SlSimParameterException('No default parameters found in module '\
                                     f'\"{modpath[-2]}\"')
     try:
         obj_defaults = getattr(defaults, obj_name)
