c45e4b4d4ade79792cb2a6b2488406df5e62e1e0
Xianzhe “TZ” Tang (唐贤哲) <122687108+trivialTZ@users.noreply.github.com>
2023-12-19 21:15:53 -0500

    Update Kext Gext los into lens.py

---
+++ slsim/ParamDistributions/kext_gext_distributions.py
@@ -0,0 +1,72 @@
+import h5py
+import numpy as np
+
+class LineOfSightDistribution:
+    joint_distributions_data = None
+    kg_distributions_data = None
+
+    def __init__(self, joint_distributions_path=None, kg_distributions_path=None):
+        """
+        Initialize the DataReader. Load data into class variables if not already loaded.
+
+        :param joint_distributions_path: Path to the 'joint_distributions.h5' file.
+        :param kg_distributions_path: Path to the 'kg_distributions_nolos.h5' file.
+        """
+        if joint_distributions_path is None:
+            joint_distributions_path = '/Users/tz/Documents/GitHub/slsim/data/glass/joint_distributions.h5'
+        if kg_distributions_path is None:
+            kg_distributions_path = '/Users/tz/Documents/GitHub/slsim/data/glass/kg_distributions_nolos.h5'
+        if LineOfSightDistribution.joint_distributions_data is None and joint_distributions_path is not None:
+            LineOfSightDistribution.joint_distributions_data = self._load_data(joint_distributions_path)
+
+        if LineOfSightDistribution.kg_distributions_data is None and kg_distributions_path is not None:
+            LineOfSightDistribution.kg_distributions_data = self._load_data(kg_distributions_path)
+
+    @staticmethod
+    def _load_data(file_path):
+        """
+        Load data from an H5 file into memory.
+
+        :param file_path: Path to the H5 file.
+        :return: Dictionary of datasets.
+        """
+        data = {}
+        with h5py.File(file_path, 'r') as h5_file:
+            for dataset_name in h5_file:
+                data[dataset_name] = h5_file[dataset_name][()]
+        return data
+
+    @staticmethod
+    def _round_to_nearest_0_1(value):
+        """
+        Round the value to the nearest 0.1.
+
+        :param value: A floating point number.
+        :return: Rounded number to the nearest 0.1.
+        """
+        if value>4.9:
+            return 4.9
+        if value<0.1:
+            return 0.1
+        return round(value * 10) / 10
+
+    def get_kappa_gamma(self, z_source, z_lens, use_kg_nolos=False):
+        """
+        Retrieve kappa and gamma values from the loaded data based on source and lens redshifts.
+
+        :param z_source: Source redshift (zs).
+        :param z_lens: Lens redshift (zd).
+        :param use_kg_nolos: Boolean to use 'kg_distributions_nolos.h5' file.
+        :return: Tuple of gamma and kappa values.
+        """
+
+        z_source_rounded = self._round_to_nearest_0_1(z_source)
+        z_lens_rounded = self._round_to_nearest_0_1(z_lens)
+        dataset_name = f'zs_{z_source_rounded}' if use_kg_nolos else f'zs_{z_source_rounded}_zd_{z_lens_rounded}'
+        data = LineOfSightDistribution.kg_distributions_data if use_kg_nolos else LineOfSightDistribution.joint_distributions_data
+        if dataset_name in data:
+            gamma = np.random.choice(data[dataset_name][:, 1])
+            kappa = np.random.choice(data[dataset_name][:, 0])
+            return gamma, kappa
+        else:
+            raise ValueError(f'No data found for zs={z_source_rounded} and zd={z_lens_rounded}.')
