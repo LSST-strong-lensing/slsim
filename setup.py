e1bffbda802e48470fefead06608f8150dabe546
felipe <fjurcelay@uc.cl>
2024-01-19 15:45:06 -0300

    requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,15 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = []
+requirements = [
+    "skypy",
+    "astropy>=5.2",
+    "numpy",
+    "scipy",
+    "speclite",
+    "pyyaml",
+    "matplotlib"
+]
 
 test_requirements = [
     "pytest>=3",
