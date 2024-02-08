cb0d08a9189ffd030fc3f06033b61c4433ee1998
narayan <nkhadka@ksu.edu>
2024-02-08 11:55:23 -0500

    minor change

--- tests/test_lens_pop.py
+++ tests/test_lens_pop.py
@@ -8,8 +8,12 @@ import numpy as np
 @pytest.fixture
 def gg_lens_pop_instance():
     cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
-    sky_area = Quantity(value=0.1, unit="deg2")
-    return LensPop(sky_area=sky_area, cosmo=cosmo)
+    sky_area = Quantity(value=0.05, unit="deg2")
+    kwargs_deflector_cut = {"band": "g", "band_max": 28, "z_min": 0.01, "z_max": 2.5}
+    kwargs_source_cut = {"band": "g", "band_max": 28, "z_min": 0.1, "z_max": 5.0}
+    return LensPop(sky_area=sky_area, cosmo=cosmo, 
+                   kwargs_deflector_cut=kwargs_deflector_cut,
+    kwargs_source_cut=kwargs_source_cut)
 
 
 def test_pes_lens_pop_instance():
@@ -76,8 +80,9 @@ def test_num_sources_tested_and_test_area(gg_lens_pop_instance):
 
 
 def test_draw_population(gg_lens_pop_instance):
-    kwargs_lens_cuts = {}
-    gg_lens_population = gg_lens_pop_instance.draw_population(kwargs_lens_cuts)
+    kwargs_lens_cuts = {"mag_arc_limit": {"g": 28}}
+    gg_lens_population = gg_lens_pop_instance.draw_population(
+        kwargs_lens_cuts=kwargs_lens_cuts)
     assert isinstance(gg_lens_population, list)
 
 
