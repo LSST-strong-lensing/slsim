ab289355e941f977037c5c2729e9094111dcad63
narayan <nkhadka@ksu.edu>
2024-02-19 19:32:06 -0500

    added a test function in test_galaxies.py

--- tests/test_Source/test_galaxies.py
+++ tests/test_Source/test_galaxies.py
@@ -54,8 +54,47 @@ class TestGalaxies(object):
                 "dec_off",
             ),
         )
+
+        gal_list2 = Table(
+            [
+                [0.5, 0.5, 0.5],
+                [1, 1, 1],
+                [4, 4, 4],
+                [0.28254256, 0.28254256, 0.28254256],
+                [0.17744173, 0.17744173, 0.17744173],
+                [0.2091449, 0.2091449, 0.2091449],
+                [0.14535427, 0.14535427, 0.14535427	],
+                [0.9724, 0.9724, 0.9724],
+                [0.0276, 0.0276, 0.0276],
+                [23, 23, 23],
+                [-0.1781825499400982, -0.1781825499400982, -0.1781825499400982],
+                [0.2665196551390636, 0.2665196551390636, 0.2665196551390636],
+            ],
+            names=(
+                "z",
+                "n0",
+                "n1",
+                "a0",
+                "a1",
+                "b0",
+                "b1",
+                "w0",
+                "w1",
+                "mag_i",
+                "ra_off",
+                "dec_off",
+            ),
+        )
         self.galaxies2 = Galaxies(
         galaxy_list=gal_list,
+        kwargs_cut={},
+        cosmo=self.cosmo,
+        sky_area=sky_area,
+        sersic_profile="double",
+        list_type="astropy_table",
+    )
+        self.galaxies3 = Galaxies(
+        galaxy_list=gal_list2,
         kwargs_cut={},
         cosmo=self.cosmo,
         sky_area=sky_area,
@@ -72,9 +111,12 @@ class TestGalaxies(object):
         assert len(galaxy) > 0
     
     def test_draw_source_double_sersic(self):
-        galaxy = self.galaxies2.draw_source()
-        assert galaxy["n_sersic_0"] == 1
-        assert galaxy["n_sersic_1"] == 4
+        galaxy1 = self.galaxies2.draw_source()
+        galaxy2 = self.galaxies3.draw_source()
+        assert galaxy1["n_sersic_0"] == 1
+        assert galaxy1["n_sersic_1"] == 4
+        assert galaxy2["n_sersic_0"] == 1
+        assert galaxy2["n_sersic_1"] == 4
 
 
 def test_galaxy_projected_eccentricity():
