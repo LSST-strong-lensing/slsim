605317e9ab70205c4248ad0075bc3410a53f8d2f
Nikki <nikki_1510@hotmail.com>
2024-03-08 11:32:42 +0000

    Changed bare except clauses

--- slsim/lsst_science_pipeline.py
+++ slsim/lsst_science_pipeline.py
@@ -761,7 +761,7 @@ def opsim_time_series_images_data(ra_list, dec_list, delta_pix, size=101, moffat
     # Import opsimsummary
     try:
         from opsimsummary import SynOpSim
-    except:
+    except ImportError:
         raise ImportError("Users need to have the right branch of opsimsummary installed (Issue#325/proposalTables)")
 
     # Initialise opsimsummary with opsim database
@@ -770,7 +770,7 @@ def opsim_time_series_images_data(ra_list, dec_list, delta_pix, size=101, moffat
 
         synopsim = SynOpSim.fromOpSimDB(opsim_path, opsimversion="fbsv2", usePointingTree=True,
                                         use_proposal_table=False, subset="unique_all")
-    except:
+    except FileNotFoundError:
         raise FileNotFoundError("Users need to have an opsim database downloaded at ../data/OpSim_database/opsim.db")
 
     # Collect observations that cover the coordinates in ra_list and dec_list
