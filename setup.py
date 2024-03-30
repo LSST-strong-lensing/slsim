658a8c4bbbb592e5259dc2848c96238634d41fba
felipe <fjurcelay@uc.cl>
2024-03-29 21:02:17 -0300

    Revert "requirements"
    This reverts commit e1bffbda802e48470fefead06608f8150dabe546.
    
    I've modified setup.py and the requirements for easyer installation
    on a non-LSST environment (local), but now I revert this as is not
    related with the purpose of this fork.

--- setup.py
+++ setup.py
@@ -10,15 +10,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [
-    "skypy",
-    "astropy>=5.2",
-    "numpy",
-    "scipy",
-    "speclite",
-    "pyyaml",
-    "matplotlib"
-]
+requirements = []
 
 test_requirements = [
     "pytest>=3",
