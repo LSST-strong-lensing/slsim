9b10814273dcc5a22215b4fe3a4cf68ee8d1cfaa
Xianzhe “TZ” Tang (唐贤哲) <122687108+trivialTZ@users.noreply.github.com>
2024-04-09 20:07:41 -0400

    confilct

--- slsim/lens.py
+++ slsim/lens.py
@@ -308,24 +308,6 @@ class Lens(LensedSystemBase):
         """
         return self.source.redshift
 
-    @property
-    def external_convergence(self):
-        """
-
-        :return: external convergence
-        """
-        _, _, kappa_ext = self.los_linear_distortions
-        return kappa_ext
-
-    @property
-    def external_shear(self):
-        """
-
-        :return: the absolute external shear
-        """
-        gamma1, gamma2, _ = self.los_linear_distortions
-        return (gamma1**2 + gamma2**2) ** 0.5
-
     def einstein_radius_deflector(self):
         """Einstein radius, from SIS approximation (coming from velocity dispersion)
         without line-of-sight correction.
@@ -702,7 +684,7 @@ class Lens(LensedSystemBase):
                 % self.deflector.deflector_type
             )
         # adding line-of-sight structure
-        gamma1, gamma2, kappa_ext = self.los_linear_distortions
+        gamma1, gamma2, kappa_ext = self.los_linear_distortions()
         kwargs_lens.append({"gamma1": gamma1, "gamma2": gamma2, "ra_0": 0, "dec_0": 0})
         kwargs_lens.append({"kappa": kappa_ext, "ra_0": 0, "dec_0": 0})
         lens_mass_model_list.append("SHEAR")
