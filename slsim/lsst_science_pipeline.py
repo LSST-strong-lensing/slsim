1b2ff9e34c3a347d40e616dfb1447be972c64f5e
Phil Holloway <54064261+P-1884@users.noreply.github.com>
2024-04-19 16:46:12 +0100

    Cosmetic change

--- slsim/lsst_science_pipeline.py
+++ slsim/lsst_science_pipeline.py
@@ -13,7 +13,6 @@ import matplotlib.pyplot as plt
 from slsim.image_simulation import point_source_coordinate_properties
 from slsim.Util.param_util import random_ra_dec
 import h5py
-# import os
 try:
     import lsst.geom as geom
     from lsst.pipe.tasks.insertFakes import _add_fake_sources
