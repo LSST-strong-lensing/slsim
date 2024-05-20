5183b1b31498e8e41745c35d488275322b5128bf
narayan <nkhadka@ksu.edu>
2024-05-20 11:26:01 -0400

    changed Quasar class to point source class.

--- slsim/lens_pop.py
+++ slsim/lens_pop.py
@@ -155,13 +155,13 @@ class LensPop(LensedPopulationBase):
             )
             self._source_model_type = "extended"
         elif source_type == "quasars":
-            from slsim.Sources.quasars import Quasars
+            from slsim.Sources.quasars import PointSources
             from slsim.Sources.QuasarCatalog.simple_quasar import quasar_catalog_simple
 
             if kwargs_quasars is None:
                 kwargs_quasars = {}
             quasar_source = quasar_catalog_simple(**kwargs_quasars)
-            self._sources = Quasars(
+            self._sources = PointSources(
                 quasar_source,
                 cosmo=cosmo,
                 sky_area=sky_area,
