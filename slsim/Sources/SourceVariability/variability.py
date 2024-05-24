26bafd1d3093063bdd3e8d02fe6239d405359258
Henry-Best-01 <henrygamemaster@aol.com>
2024-05-24 07:59:19 -0400

    Removing extra functions from variability class, now variability class calls them using itself as the argument.

--- slsim/Sources/SourceVariability/variability.py
+++ slsim/Sources/SourceVariability/variability.py
@@ -35,10 +35,14 @@ class Variability(object):
                     2) ('time_array') and ('magnitude_array') to define the driving signal
                 - ('redshift') to bring observer frame wavelengths to the rest frame
                 - ('delta_wavelength') to change the resolution (in nm) of a speclite filter
-                - ('obs_frame_wavelength_in_nm') list of observer frame wavelengths in nm
-                - ('rest_frame_wavelength_in_nm') list of rest frame wavelengths in nm
-                - ('speclite_filter') list of speclite filter names to use
-                - ('response_function') list of response functions to use (see notebook for samples)
+                - one of the following four options:
+                    1) ('obs_frame_wavelength_in_nm') observer frame wavelength in nm
+                    2) ('rest_frame_wavelength_in_nm') rest frame wavelength in nm
+                    3) ('speclite_filter') speclite filter name to use
+                    4) ('response_function') user defined response function to use
+                        (see notebook for samples)
+                    Note that this is the order keywords are checked in, and only
+                    the first keyword will be used!
 
         :type kwargs_variability_model: dict
         """
@@ -120,129 +124,137 @@ class Variability(object):
         """
         return self._model(observation_times)
 
-    def parse_kwargs_for_lamppost_reprocessed_model(self):
-        """Separates categories of self.kwargs for lamppost reprocessing.
 
-        :return: Dictionaries for:
-            - agn kwargs
-            - driving signal kwargs
-            - reprocessing kwargs
-            - signal kwargs
-        """
-        self.agn_kwargs = {}
-        self.driving_signal_kwargs = {}
-        self.reprocessing_kwargs = {}
-        self.signal_kwargs = {}
+def parse_kwargs_for_lamppost_reprocessed_model(variability):
+    """Separates categories of variability.kwargs for lamppost reprocessing.
 
-        self.redshift = 0
-        self.delta_wavelength = 50
+    :param variability: Variability class object following model 'lamppost_reprocessed'.
+    :result: Parses variability.kwargs into variability dictionaries for:
+        - agn kwargs
+        - driving signal kwargs
+        - reprocessing kwargs
+        - signal kwargs
+    """
+    variability.agn_kwargs = {}
+    variability.driving_signal_kwargs = {}
+    variability.reprocessing_kwargs = {}
+    variability.signal_kwargs = {}
 
-        for kwarg in self.kwargs_model:
-            if kwarg in [
-                "r_out",
-                "corona_height",
-                "r_resolution",
-                "inclination_angle",
-                "black_hole_mass_exponent",
-                "black_hole_spin",
-                "eddington_ratio",
-            ]:
-                self.agn_kwargs[kwarg] = self.kwargs_model[kwarg]
+    variability.redshift = 0
+    variability.delta_wavelength = 50
 
-            elif kwarg in ["time_array", "magnitude_array"]:
-                self.signal_kwargs[kwarg] = self.kwargs_model[kwarg]
+    for kwarg in variability.kwargs_model:
+        if kwarg in [
+            "r_out",
+            "corona_height",
+            "r_resolution",
+            "inclination_angle",
+            "black_hole_mass_exponent",
+            "black_hole_spin",
+            "eddington_ratio",
+        ]:
+            variability.agn_kwargs[kwarg] = variability.kwargs_model[kwarg]
 
-            elif kwarg in [
-                "length_of_light_curve",
-                "time_resolution",
-                "log_breakpoint_frequency",
-                "low_frequency_slope",
-                "high_frequency_slope",
-                "mean_magnitude",
-                "standard_deviation",
-                "normal_magnitude_variance",
-                "zero_point_mag",
-                "input_frequencies",
-                "input_psd",
-                "light_curve",
-                "seed",
-                "amp",
-                "freq",
-            ]:
-                self.driving_signal_kwargs[kwarg] = self.kwargs_model[kwarg]
+        elif kwarg in ["time_array", "magnitude_array"]:
+            variability.signal_kwargs[kwarg] = variability.kwargs_model[kwarg]
 
-            elif kwarg in ["redshift"]:
-                self.redshift = self.kwargs_model[kwarg]
-            elif kwarg in ["delta_wavelength"]:
-                self.delta_wavelength = self.kwargs_model[kwarg]
-            else:
-                self.reprocessing_kwargs[kwarg] = self.kwargs_model[kwarg]
+        elif kwarg in [
+            "length_of_light_curve",
+            "time_resolution",
+            "log_breakpoint_frequency",
+            "low_frequency_slope",
+            "high_frequency_slope",
+            "mean_magnitude",
+            "standard_deviation",
+            "normal_magnitude_variance",
+            "zero_point_mag",
+            "input_frequencies",
+            "input_psd",
+            "light_curve",
+            "seed",
+            "amp",
+            "freq",
+        ]:
+            variability.driving_signal_kwargs[kwarg] = variability.kwargs_model[kwarg]
 
-    def reprocess_with_lamppost_model(self):
-        """Reprocesses the signal based on the type of accretion disk reprocessor.
+        elif kwarg in ["redshift"]:
+            variability.redshift = variability.kwargs_model[kwarg]
+        elif kwarg in ["delta_wavelength"]:
+            variability.delta_wavelength = variability.kwargs_model[kwarg]
+        else:
+            variability.reprocessing_kwargs[kwarg] = variability.kwargs_model[kwarg]
 
-        :return: dict containing a light curve object parameters
-        """
 
-        if "obs_frame_wavelength_in_nm" in self.reprocessing_kwargs:
-            wavelength = self.reprocessing_kwargs["obs_frame_wavelength_in_nm"]
-            rest_wavelength = wavelength / (1 + self.redshift)
+def reprocess_with_lamppost_model(variability):
+    """Reprocesses the signal based on the type of accretion disk reprocessor.
 
-            reprocessed_signal = self.accretion_disk_reprocessor.reprocess_signal(
-                rest_frame_wavelength_in_nanometers=rest_wavelength
+    :param variability: Variability class object model 'lamppost_reprocessed'.
+    :return: dict containing a light curve object parameters
+    """
+
+    if "obs_frame_wavelength_in_nm" in variability.reprocessing_kwargs:
+        wavelength = variability.reprocessing_kwargs["obs_frame_wavelength_in_nm"]
+        rest_wavelength = wavelength / (1 + variability.redshift)
+
+        reprocessed_signal = variability.accretion_disk_reprocessor.reprocess_signal(
+            rest_frame_wavelength_in_nanometers=rest_wavelength
+        )
+        light_curve = {
+            "MJD": variability.signal_kwargs["time_array"],
+            "ps_mag_" + str(wavelength)[:5]: reprocessed_signal,
+        }
+
+    elif "rest_frame_wavelength_in_nm" in variability.reprocessing_kwargs:
+        rest_wavelength = variability.reprocessing_kwargs["rest_frame_wavelength_in_nm"]
+        reprocessed_signal = variability.accretion_disk_reprocessor.reprocess_signal(
+            rest_frame_wavelength_in_nanometers=rest_wavelength
+        )
+        light_curve = {
+            "MJD": variability.signal_kwargs["time_array"],
+            "ps_mag_" + str(rest_wavelength)[:5]: reprocessed_signal,
+        }
+
+    elif "speclite_filter" in variability.reprocessing_kwargs:
+        speclite_filter = variability.reprocessing_kwargs["speclite_filter"]
+        response_function = (
+            variability.accretion_disk_reprocessor.define_passband_response_function(
+                speclite_filter,
+                redshift=variability.redshift,
+                delta_wavelength=variability.delta_wavelength,
             )
-            light_curve = {
-                "MJD": self.signal_kwargs["time_array"],
-                "ps_mag_" + str(wavelength)[:5]: reprocessed_signal,
-            }
-
-        elif "rest_frame_wavelength_in_nm" in self.reprocessing_kwargs:
-            rest_wavelength = self.reprocessing_kwargs["rest_frame_wavelength_in_nm"]
-            reprocessed_signal = self.accretion_disk_reprocessor.reprocess_signal(
-                rest_frame_wavelength_in_nanometers=rest_wavelength
-            )
-            light_curve = {
-                "MJD": self.signal_kwargs["time_array"],
-                "ps_mag_" + str(rest_wavelength)[:5]: reprocessed_signal,
-            }
-
-        elif "speclite_filter" in self.reprocessing_kwargs:
-            speclite_filter = self.reprocessing_kwargs["speclite_filter"]
-            response_function = (
-                self.accretion_disk_reprocessor.define_passband_response_function(
-                    speclite_filter,
-                    redshift=self.redshift,
-                    delta_wavelength=self.delta_wavelength,
-                )
-            )
-            reprocessed_signal = self.accretion_disk_reprocessor.reprocess_signal(
-                response_function_amplitudes=response_function
-            )
-            light_curve = {
-                "MJD": self.signal_kwargs["time_array"],
-                "ps_mag_" + str(speclite_filter): reprocessed_signal,
-            }
-        elif "response_function_amplitudes" in self.reprocessing_kwargs:
-            if "response_function_time_lags" not in self.reprocessing_kwargs:
-                reprocessed_signal = self.accretion_disk_reprocessor.reprocess_signal(
-                    response_function_amplitudes=self.reprocessing_kwargs[
+        )
+        reprocessed_signal = variability.accretion_disk_reprocessor.reprocess_signal(
+            response_function_amplitudes=response_function
+        )
+        light_curve = {
+            "MJD": variability.signal_kwargs["time_array"],
+            "ps_mag_" + str(speclite_filter): reprocessed_signal,
+        }
+    elif "response_function_amplitudes" in variability.reprocessing_kwargs:
+        if "response_function_time_lags" not in variability.reprocessing_kwargs:
+            reprocessed_signal = (
+                variability.accretion_disk_reprocessor.reprocess_signal(
+                    response_function_amplitudes=variability.reprocessing_kwargs[
                         "response_function_amplitudes"
                     ]
                 )
-            else:
-                reprocessed_signal = self.accretion_disk_reprocessor.reprocess_signal(
-                    response_function_amplitudes=self.reprocessing_kwargs[
+            )
+        else:
+            reprocessed_signal = (
+                variability.accretion_disk_reprocessor.reprocess_signal(
+                    response_function_amplitudes=variability.reprocessing_kwargs[
                         "response_function_amplitudes"
                     ],
-                    response_function_time_lags=self.reprocessing_kwargs[
+                    response_function_time_lags=variability.reprocessing_kwargs[
                         "response_function_time_lags"
                     ],
                 )
-            light_curve = {
-                "MJD": self.signal_kwargs["time_array"],
-                "ps_mag_user": reprocessed_signal,
-            }
-        else:
-            raise ValueError("Please provide a reprocessing method")
+            )
 
-        return light_curve
+        light_curve = {
+            "MJD": variability.signal_kwargs["time_array"],
+            "ps_mag_user": reprocessed_signal,
+        }
+    else:
+        raise ValueError("Please provide a reprocessing method")
+    return light_curve
