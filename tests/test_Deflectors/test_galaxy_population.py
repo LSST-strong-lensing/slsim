4888841b341041f5b8c495889aab41b06a8ee237
阿部克哉 <ktabe@KAbeMac-mini.local>
2024-06-03 15:13:54 +0900

    change test_galaxy_size() to test for massive galaxies

--- tests/test_Deflectors/test_galaxy_population.py
+++ tests/test_Deflectors/test_galaxy_population.py
@@ -1,14 +1,13 @@
 import numpy as np
 import pytest
 from colossus.cosmology import cosmology
-from slsim.Deflectors.galaxy_population import (
-    galaxy_size,
-    set_gals_param,
-    gals_init,
-    stellarmass_halomass,
-    modelscLognormal,
-)
 from scipy import stats
+
+from slsim.Deflectors.galaxy_population import galaxy_size
+from slsim.Deflectors.galaxy_population import gals_init
+from slsim.Deflectors.galaxy_population import modelscLognormal
+from slsim.Deflectors.galaxy_population import set_gals_param
+from slsim.Deflectors.galaxy_population import stellarmass_halomass
 
 
 def test_galaxy_size():
@@ -16,53 +15,37 @@ def test_galaxy_size():
     cosmo = cosmology.setCosmology("planck18")
     hubble = cosmo.H0 / 100.0
     mh = 1e12  # Example dark matter halo mass in units of Msun/h
-    mstar = 1e10 * hubble  # Example stellar mass in units of Msun/h
+    mstar = 10**10.8 * hubble  # Example stellar mass in units of Msun/h
     z = 0.75  # Example redshift
     q_out = "rb"  # Output query in units of Mpc/h
     model = "vdW23"  # Model to use
 
     # Verify the correct result is obtained
-    expected_approx_size = (
-        10**0.125 * hubble / 1e3 * 0.551
-    )  # from van der Wel et al. 2023 Fig. 10 in units of Mpc/h
-    calculated_size = galaxy_size(
-        mh, mstar, z, cosmo, q_out, model, scatter=False
-    )  #  in units of Mpc/h
+    expected_approx_size = 10**0.21 * hubble / 1e3 * 0.551  # from van der Wel et al. 2023 Fig. 10 in units of Mpc/h
+    calculated_size = galaxy_size(mh, mstar, z, cosmo, q_out, model, scatter=False)  #  in units of Mpc/h
 
-    assert (
-        pytest.approx(calculated_size, rel=0.1) == expected_approx_size
-    ), "The galaxy size should match the expected value."
+    assert pytest.approx(calculated_size, rel=0.1) == expected_approx_size, "The galaxy size should match the expected value."
 
     # Verify the correct scatter is obtained
     n_ar = 1000
     mh_ar = np.array([1e12] * n_ar)
-    calculated_size_w_scatters = galaxy_size(
-        mh_ar, mstar, z, cosmo, q_out, model, scatter=True
-    )
+    calculated_size_w_scatters = galaxy_size(mh_ar, mstar, z, cosmo, q_out, model, scatter=True)
     logscatters = np.log(calculated_size_w_scatters / calculated_size)
     # Perform the KS test to see if the logged data follows a normal distribution
-    D, p_value = stats.kstest(
-        logscatters, "norm", args=(logscatters.mean(), logscatters.std(ddof=1))
-    )
+    D, p_value = stats.kstest(logscatters, "norm", args=(logscatters.mean(), logscatters.std(ddof=1)))
     # Set the significance level at which you are testing
     alpha = 0.05  # Commonly used significance level
-    assert (
-        p_value > alpha
-    ), f"Sample data does not follow a lognormal distribution (p-value={p_value})"
+    assert p_value > alpha, f"Sample data does not follow a lognormal distribution (p-value={p_value})"
 
     # test_galaxy_size_unknown_model
     with pytest.raises(Exception) as excinfo:
         galaxy_size(1e12, 1e11, 0.3, cosmo, model="unknown_model")
-    assert "Unknown model" in str(
-        excinfo.value
-    ), "An exception with 'Unknown model' message should be raised for unknown models."
+    assert "Unknown model" in str(excinfo.value), "An exception with 'Unknown model' message should be raised for unknown models."
 
     # test_galaxy_size_unknown_model
     with pytest.raises(Exception) as excinfo:
         galaxy_size(1e12, 1e11, 0.3, cosmo, q_out="unknown_output")
-    assert "Unknown output" in str(
-        excinfo.value
-    ), "An exception with 'Unknown model' message should be raised for unknown output"
+    assert "Unknown output" in str(excinfo.value), "An exception with 'Unknown model' message should be raised for unknown output"
 
     # test_mh_is_not_float/list/nd.array_case
     with pytest.raises(Exception) as excinfo:
@@ -74,21 +57,13 @@ def test_galaxy_size():
     # Test the mean galaxy_size with valid arguments to ensure correct behavior for orugi20 model
     model2 = "oguri20"  # Model to use
     sig_tb = 0.2
-    calculated_size_model2 = galaxy_size(
-        mh, mstar, z, cosmo, q_out, model2, scatter=True, sig_tb=sig_tb
-    )  # in units of Mpc/h
-    assert (
-        0 < calculated_size_model2 < 0.1
-    ), "The galaxy size should be less than 100 kpc"
+    calculated_size_model2 = galaxy_size(mh, mstar, z, cosmo, q_out, model2, scatter=True, sig_tb=sig_tb)  # in units of Mpc/h
+    assert 0 < calculated_size_model2 < 0.1, "The galaxy size should be less than 100 kpc"
 
     # Test the mean galaxy_size with valid arguments to ensure correct behavior for karmakar23 model
     model3 = "karmakar23"
-    calculated_size_model3 = galaxy_size(
-        mh, mstar, z, cosmo, q_out, model3, scatter=True
-    )  # in units of Mpc/h
-    assert (
-        0 < calculated_size_model3 < 0.1
-    ), "The galaxy size should be less than 100 kpc"
+    calculated_size_model3 = galaxy_size(mh, mstar, z, cosmo, q_out, model3, scatter=True)  # in units of Mpc/h
+    assert 0 < calculated_size_model3 < 0.1, "The galaxy size should be less than 100 kpc"
 
 
 def test_modelscLognormal():
@@ -107,12 +82,8 @@ def test_stellarmass_halomass():
     ratio_SMHM = stellar_masses / mh_ar
     index_of_max = np.argmax(ratio_SMHM)
     Mh_at_peak_ratio_SMHM = mh_ar[index_of_max]
-    assert (
-        pytest.approx(np.log10(Mh_at_peak_ratio_SMHM), rel=0.02) == 12
-    ), "The SMHM ratio should be peaked at Mh ~ 1e12 M_sol"
-    assert all(
-        0 <= r <= 0.1 for r in ratio_SMHM
-    ), "The SMHM conversion rate should be at most 10%"
+    assert pytest.approx(np.log10(Mh_at_peak_ratio_SMHM), rel=0.02) == 12, "The SMHM ratio should be peaked at Mh ~ 1e12 M_sol"
+    assert all(0 <= r <= 0.1 for r in ratio_SMHM), "The SMHM conversion rate should be at most 10%"
 
     # TYPE_SMHM="obs"
     paramc, params = gals_init(TYPE_SMHM="obs")
@@ -120,12 +91,8 @@ def test_stellarmass_halomass():
     ratio_SMHM = stellar_masses / mh_ar
     index_of_max = np.argmax(ratio_SMHM)
     Mh_at_peak_ratio_SMHM = mh_ar[index_of_max]
-    assert (
-        pytest.approx(np.log10(Mh_at_peak_ratio_SMHM), rel=0.02) == 12
-    ), "The SMHM ratio should be peaked at Mh ~ 1e12 M_sol"
-    assert all(
-        0 <= r <= 0.1 for r in ratio_SMHM
-    ), "The SMHM conversion rate should be at most 10%"
+    assert pytest.approx(np.log10(Mh_at_peak_ratio_SMHM), rel=0.02) == 12, "The SMHM ratio should be peaked at Mh ~ 1e12 M_sol"
+    assert all(0 <= r <= 0.1 for r in ratio_SMHM), "The SMHM conversion rate should be at most 10%"
 
     # TYPE_SMHM="true_all"
     paramc, params = gals_init(TYPE_SMHM="true_all")
@@ -133,12 +100,8 @@ def test_stellarmass_halomass():
     ratio_SMHM = stellar_masses / mh_ar
     index_of_max = np.argmax(ratio_SMHM)
     Mh_at_peak_ratio_SMHM = mh_ar[index_of_max]
-    assert (
-        pytest.approx(np.log10(Mh_at_peak_ratio_SMHM), rel=0.02) == 12
-    ), "The SMHM ratio should be peaked at Mh ~ 1e12 M_sol"
-    assert all(
-        0 <= r <= 0.1 for r in ratio_SMHM
-    ), "The SMHM conversion rate should be at most 10%"
+    assert pytest.approx(np.log10(Mh_at_peak_ratio_SMHM), rel=0.02) == 12, "The SMHM ratio should be peaked at Mh ~ 1e12 M_sol"
+    assert all(0 <= r <= 0.1 for r in ratio_SMHM), "The SMHM conversion rate should be at most 10%"
 
 
 def test_set_gals_param():
