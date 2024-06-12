d1e2f0f186d131fb0a8ee364d64a127c07480789
cmistick <cmistick@umich.edu>
2024-06-12 16:38:01 -0400

    Made requested changes and amended rate function.

--- slsim/Sources/Supernovae/supernovae_pop.py
+++ slsim/Sources/Supernovae/supernovae_pop.py
@@ -10,18 +10,15 @@ Oguri and Marshall 2010
 def calculate_star_formation_rate(z):
     """Calculates the cosmic star formation rate. (Eq 13 - Oguri and Marshall 2010)
 
-    :param z: redshift
+    :param z: redshift (z>=0)
     :return: cosmic star formation rate in [(h)yr^(-1)Mpc^(-3)]
     """
-    if z >= 0:
-        star_formation_rate = (0.0118 + 0.08 * z) / (1 + (z / 3.3) ** 5.2)
-        return star_formation_rate
-    else:
-        return np.nan
+    star_formation_rate = (0.0118 + 0.08 * z) / (1 + (z / 3.3) ** 5.2)
+    return star_formation_rate
 
 
-def t_d_power(t_d):
-    """Calculates the power law constraint on time delay. (Eq 14 - Oguri and Marshall 2010)
+def delay_time_distribution(t_d):
+    """Calculates the power-law constraint on time delay. (Eq 14 - Oguri and Marshall 2010)
 
     :param t_d: time delay
     :return: constrained time delay
@@ -36,12 +33,19 @@ def t_d_power(t_d):
 class SNIaRate(object):
     """Class to calculate supernovae rates."""
 
-    def __init__(self, cosmo):
+    def __init__(self, cosmo, z_max):
         """
         :param cosmo: cosmology object
         :type cosmo: ~astropy.cosmology object
+        :param z_max: maximum redshift to describe the SFR density (no stars accounted for >z_max)
         """
         self._cosmo = cosmo
+        self._z_max = z_max
+
+        self._t_min = self._cosmo.age(z=self._z_max).to_value()  # Time at redshift z_max
+        self._t_0 = self._cosmo.age(0).to_value()  # Time at redshift z = 0
+
+        self._denominator = integrate.quad(delay_time_distribution, 0.1, self._t_0-self._t_min)
 
     def z_from_time(self, t):
         """Calculates redshift given cosmic time.
@@ -52,13 +56,10 @@ class SNIaRate(object):
         :return: redshift at time t [float]
         """
         if not hasattr(self, "_age_inv"):
-            z_array = np.linspace(0, 1000, 100000)
+            z_array = np.linspace(0, self._z_max, 100)
             z_array = z_array[::-1]
             t_array = self._cosmo.age(z_array).to_value()
-            t_array = np.insert(t_array, 0, -20)
-            z_array = np.insert(z_array, 0, 1000000)
-            self._age_inv = interp.interp1d(t_array, z_array)
-
+            self._age_inv = interp.interp1d(t_array, z_array, fill_value="extrapolate")
         return self._age_inv(t).item()
 
     def _numerator_integrand(self, t_d, t):
@@ -66,14 +67,14 @@ class SNIaRate(object):
         (Eq 15 - Oguri and Marshall 2010)
 
         :param t_d: time delay
-        :param t: lookback time
+        :param t: time at final integrated redshift
         :return: numerator integrand
         """
         if t < 0 or t_d < 0:
             return np.nan
         else:
-            ft_d = t_d_power(t_d)
-            z_t = self.z_from_time(t - t_d)
+            ft_d = delay_time_distribution(t_d)
+            z_t = self.z_from_time(t - t_d)  # time since big bang
             return calculate_star_formation_rate(z_t) * ft_d
 
     def calculate_SNIa_rate(self, z, eta=0.04):
@@ -89,17 +90,12 @@ class SNIaRate(object):
             return np.nan
         else:
             C_Ia = 0.032
-
             t_z = self._cosmo.age(z).to_value()  # Time at given redshift z
-            t_0 = self._cosmo.age(0).to_value()  # Time at redshift z = 0
-            t = self._cosmo.lookback_time(
-                z
-            ).to_value()  # Lookback time at given redshift z
 
             numerator = integrate.quad(
-                lambda t_d: self._numerator_integrand(t_d, t), 0.1, t_z
+                self._numerator_integrand, 0.1, t_z-self._t_min, args=(t_z,)
             )
-            denominator = integrate.quad(lambda t_d: t_d_power(t_d), 0.1, t_0)
+            denominator = self._denominator
 
             SNIa_rate = eta * C_Ia * (numerator[0] / denominator[0])
             return SNIa_rate
--- tests/test_Source/test_supernovae_pop.py
+++ tests/test_Source/test_supernovae_pop.py
@@ -1,6 +1,6 @@
 from slsim.Sources.Supernovae.supernovae_pop import (
     calculate_star_formation_rate,
-    t_d_power,
+    delay_time_distribution,
 )
 from slsim.Sources.Supernovae.supernovae_pop import SNIaRate
 from astropy.cosmology import FlatLambdaCDM
@@ -8,27 +8,40 @@ import numpy as np
 import numpy.testing as npt
 
 cosmo = FlatLambdaCDM(70, 0.3)
-sne_rate = SNIaRate(cosmo=cosmo)
+sne_rate = SNIaRate(cosmo, 10)
 
 
 def test_calculate_star_formation_rate():
-    z = -1
-    npt.assert_equal(calculate_star_formation_rate(z), np.nan)
     z = 0
     npt.assert_almost_equal(calculate_star_formation_rate(z), 0.0118, decimal=4)
     z = 3
     npt.assert_almost_equal(calculate_star_formation_rate(z), 0.1565, decimal=3)
 
 
-def test_t_d_power():
+def test_delay_time_distribution():
     t_d = -1
-    npt.assert_equal(t_d_power(t_d), np.nan)
+    npt.assert_equal(delay_time_distribution(t_d), np.nan)
     t_d = 5
-    npt.assert_almost_equal(t_d_power(t_d), 0.1758, decimal=4)
+    npt.assert_almost_equal(delay_time_distribution(t_d), 5**(-1.08), decimal=4)
 
 
 def test_z_from_time():
-    z_true = 1
+    z_true = 0
+    t = cosmo.age(z_true)
+    z_est = sne_rate.z_from_time(t)
+    npt.assert_almost_equal(z_est, z_true, decimal=3)
+
+    z_true = 4
+    t = cosmo.age(z_true)
+    z_est = sne_rate.z_from_time(t)
+    npt.assert_almost_equal(z_est, z_true, decimal=3)
+
+    z_true = 7
+    t = cosmo.age(z_true)
+    z_est = sne_rate.z_from_time(t)
+    npt.assert_almost_equal(z_est, z_true, decimal=3)
+
+    z_true = 10
     t = cosmo.age(z_true)
     z_est = sne_rate.z_from_time(t)
     npt.assert_almost_equal(z_est, z_true, decimal=3)
@@ -41,12 +54,13 @@ def test_numerator_integrand():
     npt.assert_equal(sne_rate._numerator_integrand(t_d, t), np.nan)
     t_d, t = 1, 1
     npt.assert_almost_equal(
-        sne_rate._numerator_integrand(t_d, t), 8.9363e-12, decimal=4
+        sne_rate._numerator_integrand(t_d, t), 0.0002559, decimal=4
     )
 
 
 def test_calculate_SNIa_rate():
     z = -1
     npt.assert_equal(sne_rate.calculate_SNIa_rate(z), np.nan)
-    z = 1.3
-    npt.assert_almost_equal(sne_rate.calculate_SNIa_rate(z), 6.661537e-5, decimal=2)
+    z = 1
+    npt.assert_almost_equal(sne_rate.calculate_SNIa_rate(z), 0.0001191, decimal=3)
+    # (Fig 2 - Oguri and Marshall 2010)
