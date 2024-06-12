4abb11152c7b062c6063efec438ad05a21fc524a
narayan <nkhadka@ksu.edu>
2024-06-12 13:19:27 -0400

    tested uncovered lines and reduced sky area in pipeline test.

--- tests/test_Pipelines/test_skypy_pipeline.py
+++ tests/test_Pipelines/test_skypy_pipeline.py
@@ -6,8 +6,10 @@ class TestSkyPyPipeline(object):
     def setup_method(self):
         from astropy.units import Quantity
 
-        sky_area = Quantity(value=0.05, unit="deg2")
-        self.pipeline = SkyPyPipeline(skypy_config=None, sky_area=sky_area)
+        self.sky_area = Quantity(value=0.001, unit="deg2")
+        self.pipeline = SkyPyPipeline(skypy_config=None, sky_area=self.sky_area)
+        self.pipeline2 = SkyPyPipeline(skypy_config="lsst_like_old", 
+                                       sky_area=self.sky_area)
 
     def test_default_pipeline(self):
         pipeline_default = SkyPyPipeline()
@@ -16,7 +18,7 @@ class TestSkyPyPipeline(object):
 
     def test_cosmology_initialization(self):
         galaxy_cosmo0 = LambdaCDM(H0=70, Om0=0.01, Ode0=0.99)
-        pipeline0 = SkyPyPipeline(cosmo=galaxy_cosmo0)
+        pipeline0 = SkyPyPipeline(cosmo=galaxy_cosmo0, sky_area=self.sky_area)
         blue_galaxies = pipeline0.blue_galaxies
         red_galaxies = pipeline0.red_galaxies
         cosmo0_blue_number = len(blue_galaxies)
@@ -24,7 +26,7 @@ class TestSkyPyPipeline(object):
 
         # LambdaCDM model
         cosmo1 = LambdaCDM(H0=70, Om0=0.3, Ode0=0.7)
-        pipeline1 = SkyPyPipeline(cosmo=cosmo1)
+        pipeline1 = SkyPyPipeline(cosmo=cosmo1, sky_area=self.sky_area)
         blue_galaxies = pipeline1.blue_galaxies
         red_galaxies = pipeline1.red_galaxies
         # we expect the pipeline works:
@@ -38,18 +40,18 @@ class TestSkyPyPipeline(object):
 
         # check FlatwCDM model
         cosmo3 = FlatwCDM(H0=70, Om0=0.3, w0=-1.0)
-        pipeline3 = SkyPyPipeline(cosmo=cosmo3)
+        pipeline3 = SkyPyPipeline(cosmo=cosmo3, sky_area=self.sky_area)
         red_galaxies = pipeline3.red_galaxies
         assert red_galaxies[0]["z"] > 0
 
         # check w0waCDM model
         cosmo4 = w0waCDM(H0=70, Om0=0.3, Ode0=0.7, w0=-1.0, wa=0.0)
-        pipeline4 = SkyPyPipeline(cosmo=cosmo4)
+        pipeline4 = SkyPyPipeline(cosmo=cosmo4, sky_area=self.sky_area)
         blue_galaxies = pipeline4.blue_galaxies
         assert blue_galaxies[0]["z"] > 0
 
         galaxy_cosmo5 = default_cosmology.get()
-        pipeline5 = SkyPyPipeline(cosmo=galaxy_cosmo5)
+        pipeline5 = SkyPyPipeline(cosmo=galaxy_cosmo5, sky_area=self.sky_area)
         blue_galaxies5 = pipeline5.blue_galaxies
         assert blue_galaxies5[0]["z"] > 0
 
@@ -60,3 +62,5 @@ class TestSkyPyPipeline(object):
     def test_red_galaxies(self):
         red_galaxies = self.pipeline.red_galaxies
         assert red_galaxies[0]["z"] > 0
+        assert len(self.pipeline2.red_galaxies["z"]) < \
+            len(self.pipeline2.blue_galaxies["z"])
