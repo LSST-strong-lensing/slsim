05553e31c6b9d1e5f2306e03e6aa29537638054b
cmistick <cmistick@umich.edu>
2024-06-12 20:07:34 -0400

    Made requested changes to test functions.

--- tests/test_Source/test_supernovae_pop.py
+++ tests/test_Source/test_supernovae_pop.py
@@ -25,40 +25,45 @@ def test_delay_time_distribution():
     npt.assert_almost_equal(delay_time_distribution(t_d), 5 ** (-1.08), decimal=4)
 
 
-def test_z_from_time():
-    z_true = 0
-    t = cosmo.age(z_true)
-    z_est = sne_rate.z_from_time(t)
-    npt.assert_almost_equal(z_est, z_true, decimal=3)
+class test_SNIaRate:
+    def test_z_from_time():
+        z_true = 0
+        t = cosmo.age(z_true)
+        z_est = sne_rate.z_from_time(t)
+        npt.assert_almost_equal(z_est, z_true, decimal=3)
 
-    z_true = 4
-    t = cosmo.age(z_true)
-    z_est = sne_rate.z_from_time(t)
-    npt.assert_almost_equal(z_est, z_true, decimal=3)
+        z_true = 4
+        t = cosmo.age(z_true)
+        z_est = sne_rate.z_from_time(t)
+        npt.assert_almost_equal(z_est, z_true, decimal=3)
 
-    z_true = 7
-    t = cosmo.age(z_true)
-    z_est = sne_rate.z_from_time(t)
-    npt.assert_almost_equal(z_est, z_true, decimal=3)
+        z_true = 7
+        t = cosmo.age(z_true)
+        z_est = sne_rate.z_from_time(t)
+        npt.assert_almost_equal(z_est, z_true, decimal=3)
 
-    z_true = 10
-    t = cosmo.age(z_true)
-    z_est = sne_rate.z_from_time(t)
-    npt.assert_almost_equal(z_est, z_true, decimal=3)
+        z_true = 10
+        t = cosmo.age(z_true)
+        z_est = sne_rate.z_from_time(t)
+        npt.assert_almost_equal(z_est, z_true, decimal=3)
 
+    def test_numerator_integrand():
+        t_d, t = -1, 1
+        npt.assert_equal(sne_rate._numerator_integrand(t_d, t), np.nan)
+        t_d, t = 1, -1
+        npt.assert_equal(sne_rate._numerator_integrand(t_d, t), np.nan)
+        t_d, t = 1, 1
+        npt.assert_almost_equal(sne_rate._numerator_integrand(t_d, t), 0.0002559, decimal=4)
 
-def test_numerator_integrand():
-    t_d, t = -1, 1
-    npt.assert_equal(sne_rate._numerator_integrand(t_d, t), np.nan)
-    t_d, t = 1, -1
-    npt.assert_equal(sne_rate._numerator_integrand(t_d, t), np.nan)
-    t_d, t = 1, 1
-    npt.assert_almost_equal(sne_rate._numerator_integrand(t_d, t), 0.0002559, decimal=4)
-
-
-def test_calculate_SNIa_rate():
-    z = -1
-    npt.assert_equal(sne_rate.calculate_SNIa_rate(z), np.nan)
-    z = 1
-    npt.assert_almost_equal(sne_rate.calculate_SNIa_rate(z), 0.0001191, decimal=3)
-    # (Fig 2 - Oguri and Marshall 2010)
+    def test_calculate_SNIa_rate():
+        z = -1
+        npt.assert_equal(sne_rate.calculate_SNIa_rate(z), np.nan)
+        # (Fig 2 - Oguri and Marshall 2010)
+        z = 0
+        npt.assert_almost_equal(sne_rate.calculate_SNIa_rate(z), 0.000041006, decimal=3)
+        z = 1
+        npt.assert_almost_equal(sne_rate.calculate_SNIa_rate(z), 0.0001191, decimal=3)
+        z = 2
+        npt.assert_almost_equal(sne_rate.calculate_SNIa_rate(z), 0.0001349, decimal=3)
+        z = 3
+        npt.assert_almost_equal(sne_rate.calculate_SNIa_rate(z), 0.00008008, decimal=3)
