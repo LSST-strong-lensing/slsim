c0f4cd5aed654f591d888c2aa7bbf88666f8da25
cmistick <cmistick@umich.edu>
2024-06-19 16:15:59 -0400

    Added lightcone integration of supernovae density

--- slsim/Sources/Supernovae/supernovae_pop.py
+++ slsim/Sources/Supernovae/supernovae_pop.py
@@ -11,6 +11,7 @@ def calculate_star_formation_rate(z):
     """Calculates the cosmic star formation rate. (Eq 13 - Oguri and Marshall 2010)
 
     :param z: redshift (z>=0)
+
     :return: cosmic star formation rate in [(h)(M_sol)yr^(-1)Mpc^(-3)]
     """
     star_formation_rate = (0.0118 + 0.08 * z) / (1 + (z / 3.3) ** 5.2)
@@ -20,14 +21,12 @@ def calculate_star_formation_rate(z):
 def delay_time_distribution(t_d):
     """Calculates the power-law constraint on time delay. (Eq 14 - Oguri and Marshall 2010)
 
-    :param t_d: time delay
+    :param t_d: time delay (t_d>=0)
+
     :return: constrained time delay
     """
-    if t_d > 0:
-        ft_d = (t_d) ** (-1.08)
-        return ft_d
-    else:
-        return np.nan
+    ft_d = (t_d) ** (-1.08)
+    return ft_d
 
 
 class SNIaRate(object):
@@ -55,8 +54,7 @@ class SNIaRate(object):
         """Calculates redshift given cosmic time.
 
         :param t: cosmic time since big bang in [Gy]
-        :param cosmo: cosmology object
-        :type cosmo: ~astropy.cosmology object
+
         :return: redshift at time t [float]
         """
         if not hasattr(self, "_age_inv"):
@@ -70,36 +68,35 @@ class SNIaRate(object):
         """Calculates the numerator integrand to be used within calculate_SNIa_rates.
         (Eq 15 - Oguri and Marshall 2010)
 
-        :param t_d: time delay
-        :param t: time at final integrated redshift
+        :param t_d: time delay (t>=0)
+        :param t: time at final integrated redshift (t>=0)
+
         :return: numerator integrand
         """
-        if t < 0 or t_d < 0:
-            return np.nan
-        else:
-            ft_d = delay_time_distribution(t_d)
-            z_t = self.z_from_time(t - t_d)  # time since big bang
-            return calculate_star_formation_rate(z_t) * ft_d
+        ft_d = delay_time_distribution(t_d)
+        z_t = self.z_from_time(t - t_d)  # time since big bang
+        return calculate_star_formation_rate(z_t) * ft_d
 
     def calculate_SNIa_rate(self, z, eta=0.04):
         """Calculates the rate of SN Ia. (Eq 15 - Oguri and Marshall 2010)
 
-        :param z: redshift
+        :param z: redshift (z>=0)
         :param eta: canonical efficiency
-        :param cosmo: cosmology object
-        :type cosmo: ~astropy.cosmology object
+
         :return: SN Ia rate n(z) in [(h)yr^(-1)Mpc^(-3)]
+        :return type: array-like
         """
-        if z < 0:
-            return np.nan
-        else:
-            C_Ia = 0.032
-            t_z = self._cosmo.age(z).to_value()  # Time at given redshift z
+        C_Ia = 0.032
+        denominator = self._denominator
+        SNIa_rate_list = []
+
+        for i in z:
+            t_z = self._cosmo.age(i).to_value()  # Time at given redshift z
 
             numerator = integrate.quad(
                 self._numerator_integrand, 0.1, t_z - self._t_min, args=(t_z,)
             )
-            denominator = self._denominator
-
             SNIa_rate = eta * C_Ia * (numerator[0] / denominator[0])
-            return SNIa_rate
+            SNIa_rate_list.append(SNIa_rate)
+
+        return np.array(SNIa_rate_list)
