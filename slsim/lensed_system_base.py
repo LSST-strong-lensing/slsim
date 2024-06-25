3f84bbb77a5e54255a3a539f550c250552d4503f
narayan <nkhadka@ksu.edu>
2024-06-25 13:14:26 -0400

    connected recent supernovae class development to other part of the code.

--- slsim/lensed_system_base.py
+++ slsim/lensed_system_base.py
@@ -22,6 +22,7 @@ class LensedSystemBase(ABC):
         sn_absolute_mag_band=None,
         sn_absolute_zpsys=None,
         lightcurve_time=None,
+        sn_modeldir = None
     ):
         """
         :param source_dict: source properties
@@ -47,6 +48,8 @@ class LensedSystemBase(ABC):
         :param test_area: area (arc-sec^2) around lensing galaxy to be investigated
         :param lightcurve_time: observation time array for lightcurve in unit of days.
         :type lightcurve_time: array
+        :param sn_modeldir: Path to the directory containing supernova files
+        :type modeldir: str
         """
         self.source = Source(
             source_dict=source_dict,
@@ -57,6 +60,7 @@ class LensedSystemBase(ABC):
             sn_absolute_zpsys=sn_absolute_zpsys,
             cosmo=cosmo,
             lightcurve_time=lightcurve_time,
+            sn_modeldir=sn_modeldir
         )
         if deflector_kwargs is None:
             deflector_kwargs = {}
