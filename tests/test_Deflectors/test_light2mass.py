f1c812b0e9be69f114a4905b2452cfcb22accbcf
Vibhore <vibhore.negi18@gmail.com>
2024-07-24 00:05:27 +0530

    added a test function for slsim/Deflectors/light2mass.py

---
+++ tests/test_Deflectors/test_light2mass.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import numpy as np
+from slsim.Deflectors.light2mass import get_velocity_dispersion
+from astropy.cosmology import FlatLambdaCDM
+
+
+
+def test_get_velocity_dispersion():
+
+    cosmo = FlatLambdaCDM(H0=72,Om0=0.26)
+    deflector_type = 'galaxy-elliptical'
+    lsst_mags = np.array([17.636, 16.674, 16.204]).reshape(1,3)     # g,,r,i mags
+    lsst_errs = np.array([0.007, 0.005, 0.005]).reshape(1,3)        # g,r,i errors
+    redshifts = np.array([0.08496])                                 #redshift
+
+    coefficients = [0.01011,0.01920,0.05162,-0.00032,0.06555,-0.02949,0.00003,0.04040,-0.00892,
+                    -0.03068,-0.21527,0.09394]         #[c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12]
+    #coefficients = [0,0,0,0,0,0,0,0,0,0,0,0]            #in case input mags are in SDSS bands
+
+    # Get velocity dispersion using spectroscopy based relations
+    vel_disp_spec = get_velocity_dispersion(deflector_type,lsst_mags,
+                                            lsst_errs,zz=redshifts,cosmo=cosmo,
+                                            bands=["g", "r", "i"],c1=coefficients[0],
+                                            c2=coefficients[1],c3=coefficients[2],c4=coefficients[3],
+                                            c5=coefficients[4],c6=coefficients[5],c7=coefficients[6],
+                                            c8=coefficients[7],c9=coefficients[8],c10=coefficients[9],
+                                            c11=coefficients[10],c12=coefficients[11],
+                                            scaling_relation="spectroscopic")
+    
+    np.testing.assert_equal(len(vel_disp_spec),len(redshifts))
+    np.testing.assert_almost_equal(vel_disp_spec[0].nominal_value, 149, decimal=-1)
+    # the returned value should be precise within +-10 km/s
+
+    # Get velocity dispersion using weak-lensing based relations
+    vel_disp_wl = get_velocity_dispersion(deflector_type,lsst_mags,
+                                     lsst_errs, zz=redshifts,cosmo=cosmo,
+                                     bands=["g", "r", "i"], c1=coefficients[0],
+                                     c2=coefficients[1],c3=coefficients[2],c4=coefficients[3],
+                                     c5=coefficients[4],c6=coefficients[5],c7=coefficients[6],
+                                     c8=coefficients[7],c9=coefficients[8],c10=coefficients[9],
+                                     c11=coefficients[10],c12=coefficients[11],
+                                     scaling_relation='weak-lensing')
+
+    
+    np.testing.assert_equal(len(vel_disp_wl),len(redshifts))
+    np.testing.assert_almost_equal(vel_disp_wl[0].nominal_value, 143, decimal=-1)
+
+if __name__ == '__main__':
+    test_get_velocity_dispersion()
