78e72a958004bc9bb9ace08b299dd53bf73c438d
Vibhore <vibhore.negi18@gmail.com>
2024-08-02 11:27:11 +0530

    worked on suggestions from Narayan

--- slsim/Deflectors/light2mass.py
+++ slsim/Deflectors/light2mass.py
@@ -4,14 +4,14 @@ from uncertainties import ufloat
 from uncertainties import unumpy
 from astropy.modeling.models import Linear1D
 from slsim.Util.k_correction import kcorr_sdss
-
+from astropy.cosmology import FlatLambdaCDM
 """
 This module provides function to calculate the central stellar velocity dispersion of the deflector 
 (elliptical galaxies) using LSST broadband magnitudes and the redshift. It assumes the evolution of 
 galaxy luminoisity function, as discussed in Bell et al 2004, Blanton et al 2003, and uses the 
 scaling relations for L-sigma relationship from Choi et al 2007 (derived from spectroscopic 
 measurements) and Parker et al 2007 (derived from weak lensing measurements). The user has the 
-option to decide which scaling relations he wants to use, the one derived from spectroscopic
+option to decide which scaling relations he/she wants to use, the one derived from spectroscopic
 measurements or from the weak lensing measurements.
 """
 
@@ -20,8 +20,8 @@ def get_velocity_dispersion(
     deflector_type,
     lsst_mags,
     lsst_errs,
-    zz="zz",
-    cosmo="cosmo",
+    redshift,
+    cosmo=FlatLambdaCDM(H0=72, Om0=0.26),
     bands=["g", "r", "i"],
     c1=0.01011,
     c2=0.01920,
@@ -40,7 +40,7 @@ def get_velocity_dispersion(
     """
     input_params:
 
-    deflector_type: type of the foreground/ deflector, e.g., 'galaxy-elliptical'
+    deflector_type: type of the foreground/ deflector, e.g., 'elliptical'
     type: string
 
     lsst_mags: a 2D array of the lsst magnitudes of the deflector with multi-band magnitudes
@@ -55,7 +55,7 @@ def get_velocity_dispersion(
     The three bands are required to perform the k-correction in the SDSS bands. If there is some other
     way of doing k-correction directly in the LSST bands, we will need only the two g and r bands data.
 
-    zz:   a 1D array of the redshifts
+    redshift:   a 1D array of the redshifts
     type: a 1D array of floats
 
     cosmo: cosmology defined
@@ -87,7 +87,7 @@ def get_velocity_dispersion(
     .. [5] Parker et al., (2007),  arXiv:0707.1698, doi: 10.1086/521541
     """
 
-    if deflector_type != "galaxy-elliptical":
+    if deflector_type != "elliptical":
         raise KeyError("The module currently supports only elliptical galaxies.")
 
     lsst_bands = ["u", "g", "r", "i", "z", "y"]
@@ -128,56 +128,42 @@ def get_velocity_dispersion(
         # k-correct upto redshift z=0 only
         k_corrections = kcorr_sdss(
             np.array([mgSDSS, mrSDSS, miSDSS]),
-            zz,
+            redshift,
             responses=sdss_responses,
             responses_out=sdss_responses,
             band_shift=0.0,
+            redshift_range = [0,2]
         )
 
-    elif scaling_relation == "weak-lensing":
-        # k-correct upto redshift z=0.1, since the scaling relations used are at z=0.1
-        k_corrections = kcorr_sdss(
-            np.array([mgSDSS, mrSDSS, miSDSS]),
-            zz,
-            responses=sdss_responses,
-            responses_out=sdss_responses,
-            band_shift=0.1,
-        )
+        # Apply the K-correction on the SDSS magnitudes
+        mgSDSS = mgSDSS - k_corrections[:, 0]
+        mrSDSS = mrSDSS - k_corrections[:, 1]
+        miSDSS = miSDSS - k_corrections[:, 2]
 
-    else:
-        raise KeyError("Invalid input for scaling relations.")
+        ## Note: It will be better if we apply the K-correction directly on the LSST magnitudes,
+        ## but no such relation is known to me right now.
 
-    # Apply the K-correction on the SDSS magnitudes
-    mgSDSS = mgSDSS - k_corrections[:, 0]
-    mrSDSS = mrSDSS - k_corrections[:, 1]
-    miSDSS = miSDSS - k_corrections[:, 2]
+        # Use the SDSS g-band and r-band magnitudes to get the B-band apparent magnitude of the galaxy using the relation
+        # given in equation A2, Appendix, Bell et al 2004 for red galaxies. This is required only for using the relations
+        # based on spectroscopic measurements.
 
-    ## Note: It will be better if we apply the K-correction directly on the LSST magnitudes,
-    ## but no such relation is known to me right now.
+        mag_B = mgSDSS + 0.155 + 0.370 * (mgSDSS - mrSDSS)
 
-    # Use the SDSS g-band and r-band magnitudes to get the B-band apparent magnitude of the galaxy using the relation
-    # given in equation A2, Appendix, Bell et al 2004 for red galaxies. This is required only for using the relations
-    # based on spectroscopic measurements.
+        # calculates the distance luminosity using the redshift and the cosmology
+        Dlum = (cosmo.luminosity_distance(redshift) * cosmo.H(0) / 100).value
 
-    mag_B = mgSDSS + 0.155 + 0.370 * (mgSDSS - mrSDSS)
+        # Convert the apparent B-band magnitude to the absolute B-band magnitude using the redshift and cosmology defined
+        # Note that the 25 here comes since Dlum is in Mpc
+        MabsB = mag_B - 5.0 * np.log10(Dlum) - 25.0
 
-    # calculates the distance luminosity using the redshift and the cosmology
-    Dlum = (cosmo.luminosity_distance(zz) * cosmo.H(0) / 100).value
 
-    # Convert the apparent r and B-band magnitudes to the absolute r and B-band magnitudes using the redshift and cosmology
-    # defined
-    # Note that the 25 here comes since Dlum is in Mpc
-    Mabsr = mrSDSS - 5.0 * np.log10(Dlum) - 25.0
-    MabsB = mag_B - 5.0 * np.log10(Dlum) - 25.0
-
-    if scaling_relation == "spectroscopic":
-
-        """Now using the data from DEEP2 and COMBO-17 surveys, Bell et 2004 found that
+        """
+        Now using the data from DEEP2 and COMBO-17 surveys, Bell et 2004 found that
         the B-band luminosity function evolves such that characteristic magnitude MBstar
         decline by 1.5 magnitudes from z=0.0 to z=1.0. We use the same assumption here;
 
-        Hence, MBstar and zz should follow the relation, i.e., MBstar =
-        MBstar0-(zz)*1.5. where MBstar0 = MBstar(at zz=0). In our case, MBstar0 = -19.31
+        Hence, MBstar and redshift should follow the relation, i.e., MBstar =
+        MBstar0-(redshift)*1.5. where MBstar0 = MBstar(at redshift=0). In our case, MBstar0 = -19.31
         has been estimated from the mean value of the MBstar0, from Table 1, Bell et al
         2004.
         """
@@ -185,41 +171,68 @@ def get_velocity_dispersion(
         MBstar_func = Linear1D(-1.5, -19.31)
 
         # Use the above model to calculate MB* at the deflector redshift
-        MBstar = MBstar_func(zz)
+        MBstar = MBstar_func(redshift)
 
         # Calculate L/L* using the magnitude-luminosity relation
         LbyLstar = 10.0 ** (-0.4 * (MabsB - MBstar))
-        """Now use the L-sigma relation for the elliptical galaxies i.e., the Faber
+
+        """
+        Now use the L-sigma relation for the elliptical galaxies i.e., the Faber
         Jackson relation, sigma/sigma_star = (L/Lstar)**(1/alpha) and taking the sigma*
         and alpha value from Choi et al 2007, derived for early type galaxies, calculate
-        the the velocity dispersion sigma."""
+        the the velocity dispersion sigma.
+        """
         sigma_star, alpha = ufloat(161, 5), 2.32  # Choi et al 2007
 
         # Use sigma_star and alpha values to calculate the stellar velocity dispersion sigma
         sigma = sigma_star * LbyLstar ** (1 / alpha)
 
     elif scaling_relation == "weak-lensing":
-        """We assume the same assumption here (from Bell et al 2004) for decline of
+        # k-correct upto redshift z=0.1, since the scaling relations used are at z=0.1
+        k_corrections = kcorr_sdss(
+            np.array([mgSDSS, mrSDSS, miSDSS]),
+            redshift,
+            responses=sdss_responses,
+            responses_out=sdss_responses,
+            band_shift=0.1,
+            redshift_range = [0,2]
+        )
+
+        # Apply the K-correction on the SDSS magnitudes
+        mgSDSS = mgSDSS - k_corrections[:, 0]
+        mrSDSS = mrSDSS - k_corrections[:, 1]
+        miSDSS = miSDSS - k_corrections[:, 2]
+
+        # calculates the distance luminosity using the redshift and the cosmology
+        Dlum = (cosmo.luminosity_distance(redshift) * cosmo.H(0) / 100).value
+
+        # Convert the apparent r-band magnitudes to the absolute r
+        Mabsr = mrSDSS - 5.0 * np.log10(Dlum) - 25.0
+
+        """
+        We assume the same assumption here (from Bell et al 2004) for decline of
         characteristic magnitude Mrstar for r'-band,
 
-        Hence, Mrstar and zz should follow the relation, i.e., MBstar =
-        MBstar0-(zz-0.1)*1.5. where Mrstar0 = MBstar(at zz=0.1).
+        Hence, Mrstar and redshift should follow the relation, i.e., MBstar =
+        MBstar0-(redshift-0.1)*1.5. where Mrstar0 = MBstar(at redshift=0.1).
 
         In our case, Mrstar0 = -20.44 has been estimated from Table 2, Blanton et al
         2003.
         """
 
-        # Convert the sdss r-mag to r'-mag from Frei & Gunn 2003.
+        # Convert the sdss r-mag to r'-mag from Frei & Gunn 2003 (Table 3).
         # r' is a fake filter i.e., r shifted to z=0.1.
         Mabsr = Mabsr - 0.11
 
         Mrstar0 = -20.44  # calculated at redhift=0.1
         # Use the above value to calculate Mrstar at the deflector redshift
-        Mrstar = Mrstar0 - (zz - 0.1) * 1.5
+        Mrstar = Mrstar0 - (redshift - 0.1) * 1.5
 
         # Calculate L/L* using the magnitude-luminosity relation
         LbyLstar = 10.0 ** (-0.4 * (Mabsr - Mrstar))
-        """Now use the L-sigma relation and taking the sigma_star and alpha value from
+        
+        """
+        Now use the L-sigma relation and taking the sigma_star and alpha value from
         Parker et al 2007, derived using weak-lensing measurements, calculate the the
         velocity dispersion sigma."""
         # sigma_star, alpha = ufloat(142,18), 3      # Parker et al 2007
@@ -233,6 +246,10 @@ def get_velocity_dispersion(
 
         # Use sigma_star and alpha values to calculate the stellar velocity dispersion sigma
         sigma = sigma_star * LbyLstar ** (1 / alpha)
+
+    else:
+        raise KeyError("Invalid input for scaling relations.")
+
 
     # returns the calculated sigma
     # type: a 1D array of uncertainties.core.Variable
