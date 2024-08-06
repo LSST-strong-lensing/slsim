56f55ee7e78477a6c7634e076f9be1f09d76f2cc
narayan <nkhadka@ksu.edu>
2024-08-06 19:24:04 -0400

    minor change

--- tests/test_lens_pop.py
+++ tests/test_lens_pop.py
@@ -29,7 +29,7 @@ def gg_lens_pop_instance():
 def test_pes_lens_pop_instance():
     cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
     sky_area = Quantity(value=0.001, unit="deg2")
-    kwargs_deflector_cut = {"band": "g", "band_max": 23, "z_min": 0.01, "z_max": 2.5}
+    kwargs_deflector_cut = {"z_min": 0.01, "z_max": 2.5}
     kwargs_source_cut = {"band": "g", "band_max": 26, "z_min": 0.1, "z_max": 5.0}
     pes_lens_pop = LensPop(
         deflector_type="all-galaxies",
