4a6679a25a0498433780aba14ffa9d695a6cab25
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2024-08-15 10:00:22 +0200

    allow for imports via Deflectors module

---
+++ slsim/Deflectors/__init__.py
@@ -0,0 +1,5 @@
+from .all_lens_galaxies import AllLensGalaxies
+from .compound_lens_halos_galaxies import CompoundLensHalosGalaxies
+from .elliptical_lens_galaxies import EllipticalLensGalaxies
+from .deflector import Deflector
+from .deflectors_base import DeflectorsBase
