69fd74df9861f2cef5a9bd03097aaf6dabd55ab8
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2024-08-15 16:32:55 +0200

    how does ruff react to wildcard imports?

--- slsim/Deflectors/__init__.py
+++ slsim/Deflectors/__init__.py
@@ -1,7 +1,13 @@
-from .all_lens_galaxies import AllLensGalaxies as AllLensGalaxies
-from .compound_lens_halos_galaxies import (
-    CompoundLensHalosGalaxies as CompoundLensHalosGalaxies,
-)
-from .elliptical_lens_galaxies import EllipticalLensGalaxies as EllipticalLensGalaxies
-from .deflector import Deflector as Deflector
-from .deflectors_base import DeflectorsBase as DeflectorsBase
+from .all_lens_galaxies import *
+from .compound_lens_halos_galaxies import *
+from .elliptical_lens_galaxies import *
+from .deflector import *
+from .deflectors_base import *
+
+# from .all_lens_galaxies import AllLensGalaxies as AllLensGalaxies
+# from .compound_lens_halos_galaxies import (
+#     CompoundLensHalosGalaxies as CompoundLensHalosGalaxies,
+# )
+# from .elliptical_lens_galaxies import EllipticalLensGalaxies as EllipticalLensGalaxies
+# from .deflector import Deflector as Deflector
+# from .deflectors_base import DeflectorsBase as DeflectorsBase
