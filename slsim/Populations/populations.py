a84c00c6e8526cb3f3557761b54aae638314c92b
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2024-08-19 15:33:16 +0200

    updated docstring and type hinting for vel_disp_from_m_star

--- slsim/Populations/populations.py
+++ slsim/Populations/populations.py
@@ -223,24 +223,29 @@ def elliptical_projected_eccentricity(
     return e1_light, e2_light, e1_mass, e2_mass
 
 
-def vel_disp_from_m_star(m_star):
-    """Function to calculate the velocity dispersion from the staller mass using
-    empirical relation for elliptical galaxies.
+def vel_disp_from_m_star(m_star: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
+    """Calculate the velocity dispersion of a galaxy from its stellar mass using an empirical
+    power-law relation for elliptical galaxies.
 
     The power-law formula is given by:
 
     .. math::
+        V_{\\mathrm{disp}} = 10^{2.32} \\left( \\frac{M_{\\mathrm{star}}}{10^{11}
+        M_\\odot} \\right)^{0.24}
 
-         V_{\\mathrm{disp}} = 10^{2.32} \\left( \\frac{M_{\\mathrm{star}}}{10^{11}
-         M_\\odot} \\right)^{0.24}
+    Values taken from table 2 of [1]
 
-    2.32,0.24 is the parameters from [1] table 2
     [1]:Auger, M. W., et al. "The Sloan Lens ACS Survey. X. Stellar, dynamical, and
     total mass correlations of massive elliptical galaxies." The Astrophysical
     Journal 724.1 (2010): 511.
 
-    :param m_star: stellar mass in the unit of solar mass
-    :return: the velocity dispersion ("km/s")
+    Args:
+        m_star (Union[float, np.ndarray]): Stellar mass of the galaxy in solar masses.
+
+    Returns:
+        Union[float, np.ndarray]: Velocity dispersion in km/s.
     """
+
     v_disp = np.power(10, 2.32) * np.power(m_star / 1e11, 0.24)
+
     return v_disp
