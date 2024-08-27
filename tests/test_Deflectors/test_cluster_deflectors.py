ae235d8d4590348d6f7aac9731c14fb401e94cb5
furcelay <fjurcelay@uc.cl>
2024-08-27 12:42:42 -0400

    added new tests for ClusterDeflectors

--- tests/test_Deflectors/test_cluster_deflectors.py
+++ tests/test_Deflectors/test_cluster_deflectors.py
@@ -2,7 +2,7 @@ from astropy.cosmology import FlatLambdaCDM
 from slsim.Deflectors.cluster_deflectors import ClusterDeflectors
 from slsim.Pipelines.skypy_pipeline import SkyPyPipeline
 from astropy.units import Quantity
-from astropy.table import Table
+from astropy.table import Table, vstack
 import pytest
 import os
 
@@ -163,6 +163,48 @@ def test_with_centers(cluster_deflectors_input):
     )
     deflector = cluster_pop.draw_deflector()
     assert deflector["subhalos"]["center_x"][0] == 0.0
+
+
+def test_missing_magnitudes(cluster_deflectors_input):
+    cluster_catalog, members_catalog, red_galaxies = cluster_deflectors_input
+    for col in members_catalog.colnames:
+        if "mag" in col:
+            members_catalog.remove_column(col)
+    kwargs_deflector_cut = {}
+    kwargs_mass2light = {}
+    cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+    sky_area = Quantity(value=0.005, unit="deg2")
+    with pytest.raises(ValueError):
+        ClusterDeflectors(
+            cluster_catalog,
+            members_catalog,
+            red_galaxies,
+            kwargs_cut=kwargs_deflector_cut,
+            kwargs_mass2light=kwargs_mass2light,
+            cosmo=cosmo,
+            sky_area=sky_area,
+        )
+
+
+def test_long_galaxy_list(cluster_deflectors_input):
+    cluster_catalog, members_catalog, red_galaxies = cluster_deflectors_input
+    red_galaxies = vstack([red_galaxies for _ in range(30)])
+    assert len(red_galaxies) > 10000
+    kwargs_deflector_cut = {}
+    kwargs_mass2light = {}
+    cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+    sky_area = Quantity(value=0.005, unit="deg2")
+    cluster_pop = ClusterDeflectors(
+        cluster_catalog,
+        members_catalog,
+        red_galaxies,
+        kwargs_cut=kwargs_deflector_cut,
+        kwargs_mass2light=kwargs_mass2light,
+        cosmo=cosmo,
+        sky_area=sky_area,
+    )
+    num_deflectors = cluster_pop.deflector_number()
+    assert num_deflectors >= 0
 
 
 if __name__ == "__main__":
