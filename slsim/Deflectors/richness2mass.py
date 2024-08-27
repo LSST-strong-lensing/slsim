a48853d6b98446b8fb61fad5237723c5b3a29ab6
furcelay <fjurcelay@uc.cl>
2024-08-27 12:23:40 -0400

    updated documentation on richness2mass.py

--- slsim/Deflectors/richness2mass.py
+++ slsim/Deflectors/richness2mass.py
@@ -1,8 +1,28 @@
 import numpy as np
 
 
+"""
+Module to convert richness to mass using mass-richness relations. This is used in ClusterDeflector
+to assign a mass to each cluster based on its richness.
+"""
+
+
 def general_mass_richness(rich, alpha, rich_0, m_0, ln_m_scatter, min_mass=0.0):
-    """M(N) = M_0 * (N / N_0) ** alpha VAR[ln(M)] = alpha**2 / N + lnM_scatter**2."""
+    """Mass-richness relation:
+    M(N) = M_0 * (N / N_0) ** alpha VAR[ln(M)] = alpha**2 / N + lnM_scatter**2.
+    :param rich: richness
+    :type rich: float
+    :param alpha: slope
+    :type alpha: float
+    :param rich_0: pivot richness
+    :type rich_0: float
+    :param m_0: mass at pivot richness
+    :type m_0: float
+    :param ln_m_scatter: scatter in log mass
+    :type ln_m_scatter: float
+    :param min_mass: minimum mass
+    :type min_mass: float
+    """
     m = m_0 * (rich / rich_0) ** alpha
 
     ln_m_var = alpha**2 / rich + ln_m_scatter**2
@@ -13,9 +33,12 @@ def general_mass_richness(rich, alpha, rich_0, m_0, ln_m_scatter, min_mass=0.0):
 
 
 def mass_richness_simet2017(rich, min_mass=1e12):
-    """Mass-richness relation from Simet et al.
-
-    2017
+    """Mass-richness relation from
+    Simet et al. 2017
+    :param rich: richness
+    :type rich: float
+    :param min_mass: minimum mass
+    :type min_mass: float
     """
     return general_mass_richness(
         rich,
@@ -28,9 +51,12 @@ def mass_richness_simet2017(rich, min_mass=1e12):
 
 
 def mass_richness_abdullah2022(rich, min_mass=1e12):
-    """Mass-richness relation from Abdullah et al.
-
-    2022 (sred13)
+    """Mass-richness relation from Abdullah
+    et al. 2022 (sred13)
+    :param rich: richness
+    :type rich: float
+    :param min_mass: minimum mass
+    :type min_mass: float
     """
     return general_mass_richness(
         rich, alpha=0.95, rich_0=1, m_0=11.4e12, ln_m_scatter=0.25, min_mass=min_mass
@@ -38,6 +64,11 @@ def mass_richness_abdullah2022(rich, min_mass=1e12):
 
 
 def mass_richness_relation(rich, relation="Abdullah2022"):
+    """Mass-richness relation
+    :param rich: richness
+    :param relation: mass-richness relation
+    :type relation: str
+    """
     if relation == "simet2017":
         return mass_richness_simet2017(rich)
     elif relation == "Abdullah2022":
