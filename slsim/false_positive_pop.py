61be7838c4057eedd99878fe6ed0fe9924ce1371
narayan <nkhadka@ksu.edu>
2024-10-08 17:52:35 -0400

    cleaned up FalsePositivePop class

--- slsim/false_positive_pop.py
+++ slsim/false_positive_pop.py
@@ -1,25 +1,23 @@
-import sncosmo
 import numpy as np
 
 from slsim.false_positive import FalsePositive
-from typing import Optional, Union
+from typing import Optional
 from astropy.cosmology import Cosmology
 from slsim.lens import theta_e_when_source_infinity
 from slsim.Sources.source_pop_base import SourcePopBase
 from slsim.ParamDistributions.los_config import LOSConfig
 from slsim.Deflectors.deflectors_base import DeflectorsBase
-from slsim.lensed_population_base import LensedPopulationBase
+from slsim.lens_pop import draw_test_area
 
 
-class FalsePositivePop(LensedPopulationBase):
-    """Class to perform samples of lens population."""
+class FalsePositivePop(object):
+    """Class to perform samples of false positive population."""
 
     def __init__(
         self,
         elliptical_galaxy_population: DeflectorsBase,
         blue_galaxy_population: SourcePopBase,
         cosmo: Optional[Cosmology] = None,
-        sky_area: Optional[float] = None,
         los_config: Optional[LOSConfig] = None,
     ):
         """
@@ -31,157 +29,36 @@ class FalsePositivePop(LensedPopulationBase):
             los_config (Optional[LOSConfig], optional): Configuration for line of sight distribution. Defaults to None.
         """
 
-        # TODO: ADD EXCEPTION FOR DEFLECTOR AND SOURCE POP FILTER MISMATCH
         self.cosmo = cosmo
         self._lens_galaxies = elliptical_galaxy_population
         self._sources = blue_galaxy_population
-        self.sky_area = sky_area
-        
-        """self._factor_source = self.sky_area.to_value(
-            "deg2"
-        ) / self._sources.sky_area.to_value("deg2")
-        self._factor_deflector = self.sky_area.to_value(
-            "deg2"
-        ) / self._lens_galaxies.sky_area.to_value("deg2")"""
         self.los_config = los_config
         if self.los_config is None:
             self.los_config = LOSConfig()
 
-    def select_lens_at_random(self, **kwargs_lens_cut):
-        """Draw a random lens within the cuts of the lens and source, with possible
-        additional cut in the lensing configuration.
-
-        # TODO: make sure mass function is preserved, # as well as option to draw all
-        lenses within the cuts within the area
-
-        :return: Lens() instance with parameters of the deflector and lens and source
-            light
+    def draw_false_positive(self, number=1):
+        """Draw given number of false positive within the cuts of the lens and source.
+        :return: list of FalsePositive() instance with parameters of a pair of elliptical
+         and blue galaxy.
         """
-        while True:
-            source = self._sources.draw_source()
+        false_positive_population = []
+        for _ in range(number):
             lens = self._lens_galaxies.draw_deflector()
-            gg_lens = FalsePositive(
+            tolerance = 0.002
+            z_min=lens["z"] + tolerance
+            source = self._sources.draw_source(z_min=z_min)
+            test_area = draw_test_area(deflector=lens)
+            false_positive = FalsePositive(
                 deflector_dict=lens,
                 source_dict=source,
                 deflector_type=self._lens_galaxies.deflector_profile,
                 cosmo=self.cosmo,
                 source_type=self._sources.source_type,
-                light_profile=self._sources.light_profile
+                light_profile=self._sources.light_profile,
+                test_area=test_area
             )
-            if gg_lens.validity_test(**kwargs_lens_cut):
-                return gg_lens
-
-    @property
-    def deflector_number(self):
-        """Number of potential deflectors (meaning all objects with mass that are being
-        considered to have potential sources behind them)
-
-        :return: number of potential deflectors
-        """
-        return round(self._factor_deflector * self._lens_galaxies.deflector_number())
-
-    @property
-    def source_number(self):
-        """Number of sources that are being considered to be placed in the sky area
-        potentially aligned behind deflectors.
-
-        :return: number of potential sources
-        """
-        return round(self._factor_source * self._sources.source_number_selected)
-
-    def get_num_sources_tested_mean(self, testarea):
-        """Compute the mean of source galaxies needed to be tested within the test area.
-
-        num_sources_tested_mean/ testarea = num_sources/ sky_area; testarea is in units
-        of arcsec^2, f_sky is in units of deg^2. 1 deg^2 = 12960000 arcsec^2
-        """
-        num_sources = self.source_number
-        num_sources_tested_mean = (testarea * num_sources) / (
-            12960000 * self._factor_source * self._sources.sky_area.to_value("deg2")
-        )
-        return num_sources_tested_mean
-
-    def get_num_sources_tested(self, testarea=None, num_sources_tested_mean=None):
-        """Draw a realization of the expected distribution (Poisson) around the mean for
-        the number of source galaxies tested."""
-        if num_sources_tested_mean is None:
-            num_sources_tested_mean = self.get_num_sources_tested_mean(testarea)
-        num_sources_range = np.random.poisson(lam=num_sources_tested_mean)
-        return num_sources_range
-
-    def draw_population(self, kwargs_lens_cuts, speed_factor=1):
-        """Return full population list of all lenses within the area # TODO: need to
-        implement a version of it. (improve the algorithm)
-
-        :param kwargs_lens_cuts: validity test keywords
-        :param speed_factor: factor by which the number of deflectors is decreased to
-            speed up the calculations.
-        :type kwargs_lens_cuts: dict
-        :return: List of Lens instances with parameters of the deflectors and lens and
-            source light.
-        :rtype: list
-        """
-
-        # Initialize an empty list to store the Lens instances
-        gg_lens_population = []
-        # Estimate the number of lensing systems
-        num_lenses = self.deflector_number
-        # num_sources = self._source_galaxies.galaxies_number()
-        #        print(num_sources_tested_mean)
-        #        print("num_lenses is " + str(num_lenses))
-        #        print("num_sources is " + str(num_sources))
-        #        print(np.int(num_lenses * num_sources_tested_mean))
-
-        # Draw a population of galaxy-galaxy lenses within the area.
-        for _ in range(int(num_lenses / speed_factor)):
-            lens = self._lens_galaxies.draw_deflector()
-            test_area = draw_test_area(deflector=lens)
-            num_sources_tested = self.get_num_sources_tested(
-                testarea=test_area * speed_factor
-            )
-            # TODO: to implement this for a multi-source plane lens system
-            if num_sources_tested > 0:
-                n = 0
-                while n < num_sources_tested:
-                    source = self._sources.draw_source()
-                    gg_lens = Lens(
-                        deflector_dict=lens,
-                        source_dict=source,
-                        deflector_type=self._lens_galaxies.deflector_profile,
-                        variability_model=self._sources.variability_model,
-                        kwargs_variability=self._sources.kwargs_variability,
-                        sn_type=self.sn_type,
-                        sn_absolute_mag_band=self.sn_absolute_mag_band,
-                        sn_absolute_zpsys=self.sn_absolute_zpsys,
-                        cosmo=self.cosmo,
-                        test_area=test_area,
-                        source_type=self._sources.source_type,
-                        los_config=self.los_config,
-                        light_profile=self._sources.light_profile,
-                        lightcurve_time=self.lightcurve_time,
-                        sn_modeldir=self.sn_modeldir,
-                        agn_driving_variability_model=self._sources.agn_driving_variability_model,
-                        agn_driving_kwargs_variability=self._sources.agn_driving_kwargs_variability,
-                    )
-                    # Check the validity of the lens system
-                    if gg_lens.validity_test(**kwargs_lens_cuts):
-                        gg_lens_population.append(gg_lens)
-                        # if a lens system passes the validity test, code should exit
-                        # the loop. so, n should be greater or equal to
-                        # num_sources_tested which will break the while loop
-                        # (instead of this one can simply use break).
-                        n = num_sources_tested
-                    else:
-                        n += 1
-        return gg_lens_population
-
-
-def draw_test_area(deflector):
-    """Draw a test area around the deflector.
-
-    :param deflector: deflector dictionary
-    :return: test area in arcsec^2
-    """
-    theta_e_infinity = theta_e_when_source_infinity(deflector)
-    test_area = np.pi * (theta_e_infinity * 2.5) ** 2
-    return test_area
+            false_positive_population.append(false_positive)
+        if number == 1:
+            return false_positive_population[0]
+        else:
+            return false_positive_population
