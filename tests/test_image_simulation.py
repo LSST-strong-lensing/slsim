920fbe9abe4a177b6fafa8996d451db9dbae96cc
narayan <nkhadka@ksu.edu>
2024-10-29 21:24:57 -0400

    adjusted image_simulation module with Lens class changes.

--- tests/test_image_simulation.py
+++ tests/test_image_simulation.py
@@ -21,6 +21,8 @@ from slsim.image_simulation import (
     lens_image,
     lens_image_series,
 )
+from slsim.Sources.source import Source
+from slsim.Deflectors.deflector import Deflector
 import pytest
 
 
@@ -41,9 +43,19 @@ class TestImageSimulation(object):
         self.source_dict = blue_one
         self.deflector_dict = red_one
         while True:
-            gg_lens = Lens(
-                source_dict=self.source_dict,
+            self.source = Source(
+            source_dict=self.source_dict,
+            cosmo=cosmo,
+            source_type="extended",
+            light_profile="single_sersic",
+        )
+            self.deflector = Deflector(
+                deflector_type="EPL",
                 deflector_dict=self.deflector_dict,
+            )
+            gg_lens = Lens(
+                source_class=self.source,
+                deflector_class=self.deflector,
                 cosmo=cosmo,
             )
             if gg_lens.validity_test():
@@ -169,12 +181,21 @@ def pes_lens_instance():
 
     cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
     while True:
-        pes_lens = Lens(
+        source = Source(
             source_dict=source_dict,
-            deflector_dict=deflector_dict,
+            cosmo=cosmo,
             source_type="point_plus_extended",
+            light_profile="single_sersic",
             variability_model="sinusoidal",
-            kwargs_variability={"amp", "freq"},
+            kwargs_variability={"amp", "freq"}
+        )
+        deflector = Deflector(
+                deflector_type="EPL",
+                deflector_dict=deflector_dict,
+            )
+        pes_lens = Lens(
+            source_class=source,
+            deflector_class=deflector,
             cosmo=cosmo,
         )
         if pes_lens.validity_test():
