a5a54d4853239208d247abbed8b71baea4d293f0
Vibhore <vibhore.negi18@gmail.com>
2025-01-30 03:28:11 +0530

    changed paths for import

--- slsim/Deflectors/light2mass.py
+++ slsim/Deflectors/light2mass.py
@@ -3,10 +3,10 @@ import numpy as np
 from uncertainties import ufloat
 from uncertainties import unumpy
 from astropy.modeling.models import Linear1D
-from slsim.Util.k_correction import kcorr_sdss
+from k_correction import kcorr_sdss
 from astropy.cosmology import FlatLambdaCDM
 from uncertainties import unumpy
-from slsim.Util.color_transformations import *
+from color_transformations import *
 
 
 
