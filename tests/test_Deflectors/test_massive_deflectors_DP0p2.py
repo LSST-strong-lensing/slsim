3197c51670c7e690629b55d06da394a18787b103
Vibhore <vibhore.negi18@gmail.com>
2025-01-30 16:41:41 +0530

    Updated test_massive_deflectors and added DP0 massive ellipticals notebook

--- tests/test_Deflectors/test_massive_deflectors_DP0p2.py
+++ tests/test_Deflectors/test_massive_deflectors_DP0p2.py
@@ -14,7 +14,6 @@ def test_get_galaxy_parameters_from_moments():
 
     theta, r_eff, q, ellipticity = get_galaxy_parameters_from_moments(xx, xy, yy)
 
-    np.testing.assert_equal(len(theta), len(r_eff), len(q), len(ellipticity))
     np.testing.assert_almost_equal(theta, -119, decimal=-1)
     np.testing.assert_almost_equal(r_eff, 3.1324, decimal=0)
     np.testing.assert_almost_equal(q, 0.77, decimal=0)
