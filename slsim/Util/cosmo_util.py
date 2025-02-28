2b297916652e3d8afde3afac66010af848c95445
Rahul Karthik <rahul.m.karthik@gmail.com>
2025-02-28 16:53:48 -0500

    cosmo_util

--- slsim/Util/cosmo_util.py
+++ slsim/Util/cosmo_util.py
@@ -5,11 +5,15 @@ def z_scale_factor(z_old, z_new, cosmo=None):
     """Return multiplication factor for object/pixel size for moving its
     redshift from z_old to z_new.
 
-    param z_old: The original redshift of the object (float). 
-    param z_new: The redshift the object will be placed at.
-    param cosmo (astropy.cosmology.FLRW, optional): The cosmology object. 
+    :param z_old: The original redshift of the object.
+    :type z_old: float 
+    :param z_new: The redshift the object will be placed at.
+    :type z_new: float
+    :param cosmo: The cosmology object.
+    :type cosmo: astropy.cosmology 
     (Defaults to a FlatLambdaCDM model.)
-    return: The multiplicative pixel size (float).
+    :return: The multiplicative pixel size.
+    :rtype: float
     """
     # Define default cosmology if not provided
     if cosmo is None:
