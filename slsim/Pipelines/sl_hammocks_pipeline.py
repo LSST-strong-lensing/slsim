3d8bde431de5298bcddc8d27c179c9b9c5d7aa7b
narayan <nkhadka@ksu.edu>
2025-04-18 13:33:33 -0400

    added magnitude table in slhamock

--- slsim/Pipelines/sl_hammocks_pipeline.py
+++ slsim/Pipelines/sl_hammocks_pipeline.py
@@ -1,18 +1,22 @@
 import numpy as np
-from astropy.table import Table
+import os
+from astropy.table import Table, hstack
 from colossus.cosmology import cosmology
 from colossus.halo import mass_defs
-from lenstronomy.Util import constants
-
 import slsim.Deflectors.galaxy_population as galaxy_population
+import slsim
 import slsim.Deflectors.halo_population as halo_population
 import slsim.Util.param_util as util
+import tempfile
+from astropy.cosmology import default_cosmology
+from skypy.pipeline import Pipeline
 
 
 class SLHammocksPipeline:
     """Class for slhammocks configuration."""
 
-    def __init__(self, slhammocks_config=None, sky_area=None, cosmo=None):
+    def __init__(self, slhammocks_config=None, sky_area=None, cosmo=None, z_min=None,
+                 z_max=None):
         """
         :param slhammocks_config: path to the deflector population csv file for 'halo-model'
                             If None, generate the population. Not supported at this time.
@@ -26,6 +30,8 @@ class SLHammocksPipeline:
                         (e.g., FlatLambdaCDM(H0=70, Om0=0.3)).
         :type cosmo: astropy.cosmology instance or None
         """
+        path = os.path.dirname(slsim.__file__)
+        module_path, _ = os.path.split(path)
         if slhammocks_config is not None:
             table = Table.read(slhammocks_config, format="csv")
             table = table_translator_for_slsim(table, cosmo)
@@ -64,9 +70,9 @@ class SLHammocksPipeline:
             - 'ns' : The tilt of the primordial power spectrum. This parameter is required to convert from astropy.cosmology to colossus.cosmology
             """
             kwargs_population_base = {
-                "z_min": 0.01,
-                "z_max": 5.0,
-                "log10host_halo_mass_min": 11.0,
+                "z_min": z_min,
+                "z_max": z_max,
+                "log10host_halo_mass_min": 11.5,
                 "log10host_halo_mass_max": 16.0,
                 "sigma_host_halo_concentration": 0.33,
                 "sigma_central_galaxy_mass": 0.2,
@@ -82,6 +88,84 @@ class SLHammocksPipeline:
             table = table_translator_for_slsim(table, cosmo)
 
             self._pipeline = table
+        skypy_config = os.path.join(
+                module_path, "data/SkyPy/slhammock_skypy.yml"
+            )
+        with open(skypy_config, "r") as file:
+                content = file.read()
+        redshift_array=list(self._pipeline["z"].value)
+        old_z = "z: PLACEHOLDER_Z"
+        new_z = (
+            f"z: {redshift_array}"
+        )
+        content = content.replace(old_z, new_z)
+        stellar_mass_array=list(self._pipeline["stellar_mass"].value)
+        old_stellar_mass = "stellar_mass: PLACEHOLDER_MASS"
+        new_stellar_mass = (
+            f"stellar_mass: {stellar_mass_array}"
+        )
+        content = content.replace(old_stellar_mass, new_stellar_mass)
+        
+        if cosmo is not None:
+            if cosmo is default_cosmology.get():
+                pass
+            else:
+                cosmology_dict = cosmo.to_format("mapping")
+
+                cosmology_class = str(cosmology_dict.pop("cosmology", None))
+                cosmology_class_str = cosmology_class.replace(
+                    "<class '", ""
+                ).replace("'>", "")
+
+                cosmology_dict.pop("cosmology", None)
+
+                if "meta" in cosmology_dict and cosmology_dict["meta"] not in [
+                    "mapping",
+                    None,
+                ]:
+                    cosmology_dict.pop("meta", None)
+                # Reason: From Astropy:'meta:mapping or None (optional, keyword-only)'
+                # However, the dict will read out as meta: OrderedDict()
+                # which may raised error.
+
+                cosmology_dict = {
+                    k: v for k, v in cosmology_dict.items() if v is not None
+                }
+
+                cosmology_params_list = []
+                for key, value in cosmology_dict.items():
+                    if hasattr(value, "value"):
+                        value = value.value
+                    cosmology_params_list.append(f"    {key}: {value}")
+
+                cosmology_params_str = "\n".join(cosmology_params_list)
+
+                old_cosmo = "cosmology: !astropy.cosmology.default_cosmology.get []"
+                new_cosmo = (
+                    f"cosmology: !{cosmology_class_str}\n{cosmology_params_str}"
+                )
+                content = content.replace(old_cosmo, new_cosmo)
+
+        with tempfile.NamedTemporaryFile(
+            mode="w", delete=False, suffix=".yml"
+        ) as tmp_file:
+            tmp_file.write(content)
+        self._skypy_pipeline = Pipeline.read(tmp_file.name)
+        self._skypy_pipeline.execute()
+        magnitude_table=self._skypy_pipeline["halo"]
+        magnitude_table.remove_columns(['z', 'stellar_mass', 'coeff'])
+        self._final_galaxy_table = hstack([self._pipeline, magnitude_table])
+
+
+            
+    @property
+    def halo_galaxies(self):
+        """slhammock pipeline for galaxies.
+
+        :return: list of galaxies from halo model
+        :rtype: list of dict
+        """
+        return self._final_galaxy_table
 
 
 def table_translator_for_slsim(table, cosmo):
@@ -109,7 +193,7 @@ def table_translator_for_slsim(table, cosmo):
         - 'p_g': posiiton angle of the galaxy in units of degree
         - 'tb': the scale radius appreared in Hernquist profile in units of arcsec.
             This parameter relates to the commonly used galaxy effective (half-mass) radius by t_b = 0.551*theta_eff.
-        - 'angular_size': galaxy effective radius in units of radian
+        - 'angular_size': galaxy effective radius in units of arcsec
     """
     sigma8 = 0.8102  # from Planck18 TT,TE,EE+lowE+lensing+BAO best-fit value
     ns = 0.9660499  # from Planck18 TT,TE,EE+lowE+lensing+BAO best-fit value
@@ -129,7 +213,7 @@ def table_translator_for_slsim(table, cosmo):
     if "ellipticity" not in table.colnames:
         table.rename_column("e_g", "ellipticity")
     if "angular_size" not in table.colnames:
-        angular_size_in_deg = table["tb"] / 0.551 * constants.arcsec
+        angular_size_in_deg = table["tb"] / 0.551
         table.add_column(angular_size_in_deg, name="angular_size")
 
     M200_array, r200_array, c200_array = zip(
