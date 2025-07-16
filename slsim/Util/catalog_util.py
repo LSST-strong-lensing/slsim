862b85f3e22e4265aadc670be64bf1dc6f86c0d6
ahuang314 <alan.huang.1@stonybrook.edu>
2025-07-15 22:39:24 -0400

    moved some catalog util functions to catalog_util

---
+++ slsim/Util/catalog_util.py
@@ -0,0 +1,232 @@
+import os
+import numpy as np
+from astropy.table import Table, join
+from astropy.io import fits
+
+from lenstronomy.Util.param_util import ellipticity2phi_q
+
+def process_cosmos_catalog(catalog_path):
+    """This function filters out sources in the catalog so that only
+    the nearby, well-resolved galaxies with high SNR remain. Thus, we
+    perform the following cuts:
+    1. redshift < 1
+    2. apparent magnitude < 20
+    3. half light radius > 10 pixels
+
+    :param catalog_path: path to the COSMOS_23.5_training_sample directory.
+        Example: catalog_path = "/home/data/COSMOS_23.5_training_sample"
+    :type catalog_path: string
+    :return: merged astropy table with only the well-resolved galaxies
+    """
+
+    catalog1_path = os.path.join(catalog_path, "real_galaxy_catalog_23.5.fits")
+    catalog2_path = os.path.join(
+        catalog_path, "real_galaxy_catalog_23.5_fits.fits"
+    )
+    cat1 = Table.read(catalog1_path, format="fits", hdu=1)
+    cat2 = Table.read(catalog2_path, format="fits", hdu=1)
+    # These sources are excluded because they are too close to other objects
+    source_exclusion_list = [
+        79,
+        309,
+        1512,
+        5515,
+        7138,
+        7546,
+        9679,
+        14180,
+        14914,
+        19494,
+        22095,
+        28335,
+        32696,
+        32778,
+        33527,
+        34946,
+        36344,
+        38328,
+        40837,
+        41255,
+        44363,
+        44871,
+        49652,
+        51303,
+        52021,
+        55803,
+        1368,
+        1372,
+        1626,
+        5859,
+        6161,
+        6986,
+        7312,
+        8108,
+        8405,
+        9349,
+        9326,
+        9349,
+        9745,
+        9854,
+        9948,
+        10146,
+        10446,
+        11209,
+        12397,
+        14642,
+        14909,
+        15473,
+        17775,
+        17904,
+        20256,
+        20489,
+        21597,
+        21693,
+        22380,
+        23054,
+        23390,
+        23790,
+        24110,
+        24966,
+        26135,
+        27222,
+        27781,
+        28297,
+        29550,
+        30089,
+        30898,
+        30920,
+        31548,
+        32025,
+        33699,
+        35553,
+        36409,
+        36268,
+        36576,
+        37198,
+        37969,
+        38873,
+        40286,
+        40286,
+        40924,
+        41731,
+        44045,
+        45066,
+        45929,
+        45929,
+        46575,
+        47517,
+        48137,
+        49441,
+        52270,
+        52730,
+        52759,
+        52891,
+        54924,
+        54445,
+        55153,
+        10584,
+        22051,
+        22365,
+        23951,
+        42334,
+        42582,
+        51492,
+        32135,
+        37106,
+        37593,
+        38328,
+        45618,
+        47829,
+        26145,
+    ]
+
+    max_z = 1.0
+    faintest_apparent_mag = 20
+    min_flux_radius = 10.0
+
+    is_ok = np.ones(len(cat2), dtype=bool)
+    is_ok &= cat2["zphot"] < max_z
+    is_ok &= cat2["mag_auto"] < faintest_apparent_mag
+    is_ok &= cat2["flux_radius"] > min_flux_radius
+
+    # Drop any catalog indices that are in the exclusion list
+    is_ok &= np.invert(np.isin(np.arange(len(cat2)), source_exclusion_list))
+
+    filtered_catalog = join(cat1[is_ok], cat2[is_ok], keys="IDENT")
+
+    # This is the half light radius that is the geometric mean of the major and minor axis lengths
+    # calculated using sqrt(q) * R_half, where R_half is the half-light radius measured along the major axis
+    # We then convert this from units of pixels to arcseconds
+    q = filtered_catalog["sersicfit"][:, 3]
+    R_half = filtered_catalog["sersicfit"][:, 1]
+    filtered_catalog["angular_size"] = (
+        np.sqrt(q) * R_half * filtered_catalog["PIXEL_SCALE"]
+    )
+
+    # drop extraneous data
+    keep_columns = [
+        "GAL_FILENAME",
+        "GAL_HDU",
+        "PIXEL_SCALE",
+        "sersicfit",
+        "angular_size",
+    ]
+
+    for col in filtered_catalog.colnames:
+        if col not in keep_columns:
+            filtered_catalog.remove_column(col)
+
+    return filtered_catalog
+
+def match_cosmos_source(n_sersic, e1, e2, angular_size, processed_cosmos_catalog, catalog_path):
+    """This function matches the parameters in source_dict to find a
+    corresponding source in the COSMOS catalog. The parameters being
+    matched are:
+
+    1. axis ratio q
+    2. angular size
+    3. n_sersic
+
+    When many matches are found, the match with the best n_sersic is taken.
+    The COSMOS image is then rotated to match the desired angle and saved.
+
+    NOTE: To save time when generating a population of lenses, the matching is only done
+    when an image is simulated, not when this class is initialized.
+    """
+
+    # Match with COSMOS catalog based off of axis ratio
+    phi, q = ellipticity2phi_q(e1, e2)
+    matched_catalog = processed_cosmos_catalog[
+        np.abs(processed_cosmos_catalog["sersicfit"][:, 3].data - q) <= 0.1
+    ]
+
+    # Match based off of angular size
+    size_ratio = (
+        angular_size / matched_catalog["angular_size"].data
+    )
+    matched_catalog = matched_catalog[size_ratio < 1.5]
+
+    # Match based off of n_sersic
+    index = np.argsort(np.abs(matched_catalog["sersicfit"][:, 2].data - n_sersic))[
+        0
+    ]
+    matched_source = matched_catalog[index]
+
+    # load and save image
+    fname = matched_source["GAL_FILENAME"]
+    hdu = int(matched_source["GAL_HDU"])
+    path = os.path.join(catalog_path, fname)
+    with fits.open(path) as file:
+        image = file[hdu].data  # flux per pixel
+
+    # Scale the angular size of the COSMOS image so that it matches the source_dict
+    scale = (
+        matched_source["PIXEL_SCALE"]
+        * angular_size
+        / matched_source["angular_size"]
+    )
+
+    # Rotate the COSMOS image so that it matches the angle given in source_dict
+    phi = np.pi / 2 - matched_source["sersicfit"][7] - phi
+
+    return image, scale, phi
