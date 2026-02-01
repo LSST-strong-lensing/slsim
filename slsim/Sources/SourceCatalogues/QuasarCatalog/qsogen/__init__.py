98504396dcf19eb9c43bb4babb67dd5d545ccdd4
Paras Sharma <nutanstrek@gmail.com>
2026-01-31 18:57:31 -0800

    pc fix

--- slsim/Sources/SourceCatalogues/QuasarCatalog/qsogen/__init__.py
+++ slsim/Sources/SourceCatalogues/QuasarCatalog/qsogen/__init__.py
@@ -2,6 +2,3 @@
 # QSOGen is used within SLSim to simulate quasar populations with realistic SEDs.
 # Here is the link to their project page: https://github.com/MJTemple/qsogen/tree/main
 # The paper describing QSOGen is available at (Temple et al. 2021, https://arxiv.org/abs/2109.04472)
-
-from .qsosed import Quasar_sed
-from .config import params_agile
