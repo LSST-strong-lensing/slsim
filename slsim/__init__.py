643ae8b9b57ecb67eff53e2c947049ae2b4c4b43
ahuang314 <alan.huang.1@stonybrook.edu>
2026-04-03 20:11:03 -0700

    re-locate default band definitions

--- slsim/__init__.py
+++ slsim/__init__.py
@@ -2,8 +2,3 @@
 
 __author__ = """DESC/SLSC"""
 __version__ = "0.1.1"
-
-# Ordered from shortest wavelength to longest wavelength, except Roman's F146
-ROMAN_BAND_LIST = ["F062", "F087", "F106", "F129", "F158", "F184", "F213", "F146"]
-LSST_BAND_LIST = ["g", "r", "i", "z", "y"]
-EUCLID_BAND_LIST = ["VIS", "Y", "J", "H"]
