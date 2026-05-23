7550adbd78e8743cbf017137f9ca997c435cb090
Zhengjingyi0823 <157674246+Zhengjingyi0823@users.noreply.github.com>
2026-05-22 22:06:39 -0400

    Submit BNSMerger_pop.py, event_pop.py, and their test file.

---
+++ slsim/Sources/Events/BNSMerger/BNSMerger_pop.py
@@ -0,0 +1,106 @@
+import numpy as np
+import scipy.integrate as integrate
+import scipy.interpolate as interp
+
+"""References:
+  SNIa population: Oguri and Marshall 2010
+  BNS merger population: Kuwahara et al. 2025
+"""
+
+class BNSMergerRate(object):
+    """Class to calculate BNS merger rates."""
+
+    def __init__(self, cosmo, z_max):
+        """
+        :param cosmo: cosmology object
+        :type cosmo: ~astropy.cosmology object
+        :param z_max: maximum redshift to describe the SFR density (no stars accounted for >z_max)
+        """
+        self._cosmo = cosmo
+        self._z_max = z_max
+
+        self._t_min = self._cosmo.age(
+            z=self._z_max
+        ).to_value()  # Time at redshift z_max
+        self._t_0 = self._cosmo.age(0).to_value()  # Time at redshift z = 0
+
+        self.t_d_min = 0.020 # Gyr, 20 Myr
+        self.t_d_max = 13.8  # Gyr, Hubble time approximately
+
+    def calculate_star_formation_rate(self, z):
+        """Calculates the binary formation rate. (Eq 3 - Kuwahara et al. 2025)
+
+        :param z: redshift (z>=0)
+        :param z_m: peak-related redshift parameter
+        :param nu: normalization constant
+        :param a & b: observable parameters by fitting the data based on the gamma-ray burst. 
+
+        :return: binary formation rate in [(M_sol)yr^(-1)Gpc^(-3)]
+        """
+        a     = 2.80
+        b     = 2.46
+        z_m   = 1.72
+        nu    = 0.146 # in unit of M_sol * Gpc^(-3) * yr^(-1)
+
+        star_formation_rate = (nu * a * np.exp(b * (z - z_m))) / (a - b + b * np.exp(a * (z - z_m)))
+        return star_formation_rate
+    
+    def delay_time_distribution(self, t_d):
+        """Calculates the normalized time delay. (Described in text after Eq 3 - Kuwahara et al. 2025)
+    
+        :param t_d: time delay (t_d>=0) in [Gyr]
+        :param t_d_min: minimum of t_d in [Gyr], assumed as 20 Myr
+        :param t_d_max: maximum of t_d in [Gyr], assumed as Hubble time
+
+        :return: probability distribution of the time delay
+        """
+
+        ft_d = 1 / (t_d * (np.log(self.t_d_max / self.t_d_min)))
+        return ft_d 
+    
+    def z_from_time(self, t):
+        """Calculates redshift given cosmic time.
+
+        :param t: cosmic time since big bang in [Gyr]
+        :return: redshift at time t [float]
+        """
+        if not hasattr(self, "_age_inv"):
+            z_array = np.linspace(0, self._z_max, 100)
+            z_array = z_array[::-1]
+            t_array = self._cosmo.age(z_array).to_value()
+            self._age_inv = interp.interp1d(t_array, z_array, fill_value="extrapolate")
+        return self._age_inv(t).item()
+    
+    def _numerator_integrand(self, t_d, t):
+        """Calculates the numerator integrand to be used within calculate_event_rates.
+        (Eq 4 - Kuwahara et al. 2025)
+
+        :param t_d: time delay (t>=0)
+        :param t: time at final integrated redshift (t>=0)
+
+        :return: numerator integrand
+        """
+        ft_d = self.delay_time_distribution(t_d)
+        z_t = self.z_from_time(t - t_d)  # time since big bang
+        return self.calculate_star_formation_rate(z_t) * ft_d
+    
+    def calculate_event_rate(self, z):
+        """Calculates the rate of events, such as BNS merger. (Eq 4 - Kuwahara et al. 2025)
+
+        :param z: redshift (z>=0)
+
+        :return: BNS merger rate R_m(z), following the unit of R_f
+        :return type: array-like
+        """
+        BNS_rate_list = []
+
+        for i in z:
+            t_z = self._cosmo.age(i).to_value()  # Time at given redshift z
+
+            numerator = integrate.quad(
+                self._numerator_integrand, self.t_d_min, t_z - self._t_min, args=(t_z,), limit=200, epsabs=1e-6,
+                epsrel=1e-4,
+            )
+            BNS_rate_list.append(numerator[0])
+
+        return np.array(BNS_rate_list)
---
+++ tests/test_Sources/test_Events/test_event_pop.py
@@ -0,0 +1,33 @@
+from astropy.cosmology import FlatLambdaCDM
+from slsim.Sources.Events.event_pop import EventPopulation
+import numpy.testing as npt
+
+class TestEventPop:
+    def setup_method(self):
+        self.cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+        self.z_max = 10
+        self.z_array = [0, 1, 2, 3]
+
+    def test_BNSMerger_pop(self):
+        event_pop = EventPopulation(
+            model="BNS", cosmo=self.cosmo, z_max=self.z_max,
+        )
+
+        rate_array = event_pop.calculate_event_rate(self.z_array)
+
+        npt.assert_almost_equal(rate_array[0], 0.03081, decimal=3)
+        npt.assert_almost_equal(rate_array[1], 0.09757, decimal=3)
+        npt.assert_almost_equal(rate_array[2], 0.09297, decimal=3)
+        npt.assert_almost_equal(rate_array[3], 0.05915, decimal=3)
+        
+    def test_SNIa_pop(self):
+        event_pop = EventPopulation(
+            model="SNIa", cosmo=self.cosmo, z_max=self.z_max,
+        )
+
+        rate_array = event_pop.calculate_event_rate(self.z_array)
+
+        npt.assert_almost_equal(rate_array[0], 0.000041006, decimal=3)
+        npt.assert_almost_equal(rate_array[1], 0.0001191, decimal=3)
+        npt.assert_almost_equal(rate_array[2], 0.0001349, decimal=3)
+        npt.assert_almost_equal(rate_array[3], 0.00008008, decimal=3)
