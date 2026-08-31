d6fcf0bf90260557abe9f15339f0b0c09e27af8c
timedilatesme <nutanstrek@gmail.com>
2026-08-31 10:20:29 -0700

    minor changes to be able to access dicts of sources/deflectors

--- slsim/Sources/SourceCatalogues/QuasarCatalog/quasar_pop.py
+++ slsim/Sources/SourceCatalogues/QuasarCatalog/quasar_pop.py
@@ -203,8 +203,6 @@ class QuasarRate(object):
         :return: dPhi_dM value in the unit of comoving volume.
         :rtype: float or np.ndarray :unit: mag^-1 Mpc^-3
         """
-        return_scalar = np.ndim(M) == 0
-
         M = np.atleast_1d(M)
         z_value = np.atleast_1d(z_value)
 
@@ -228,7 +226,7 @@ class QuasarRate(object):
             where=denominator_dphi_dm != 0,
         )
 
-        return term1 if not return_scalar else term1.item()
+        return term1
 
     def convert_magnitude(self, magnitude, z, conversion="apparent_to_absolute"):
         """Converts between apparent and absolute magnitudes using
